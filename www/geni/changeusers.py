095ea6f49240d4038a51566295a5319c6f7bb6d4
justinc <justinc@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2008-10-18 21:34:04 +0000

    Module that adds / removes a user to a vessel

---
+++ www/geni/changeusers.py
@@ -0,0 +1,31 @@
+include time.repy
+include nmclient.repy
+include rsa.repy
+
+
+def changeusers(userpubkeylist, nmip, nmport, vesselname, nodepubkey, nodeprivkey):
+
+  try:
+    nmhandle = nmclient_createhandle(nmip, nmport)
+  except NMClientException:
+    return False
+  
+  myhandleinfo = nmclient_get_handle_info(nmhandle)
+  myhandleinfo['publickey'] = nodepubkey
+  myhandleinfo['privatekey'] = nodeprivkey
+  nmclient_set_handle_info(nmhandle, myhandleinfo)
+
+  # I need to convert the keys to strings before using join
+  userpubkeystringlist = []
+  for userpubkey in userpubkeylist:
+    userpubkeystringlist.append(rsa_publickey_to_string(userpubkey))
+    
+  # set the users...
+  try:
+    nmclient_signedsay(nmhandle, "ChangeUsers", myvessel, '|'.join(userpubkeystringlist))
+  except NMClientException:
+    return False
+
+  return True
+
+
