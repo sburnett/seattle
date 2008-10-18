7a3e240264ea3b881862d3cb3e28575f66a93b70
butaud <butaud@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2008-10-18 05:03:54 +0000

    Completely restructured dist directory

--- dist/install/README.txt
+++
@@ -1,8 +0,0 @@
-Procedure for changing Windows installer (will eventually be automated):
-1. Extract all files from win.zip into win\.
-2. Make necessary changes to install.bat or to RUNME.bat (the script that will eventually start seattle from the install directory).
-3. Run an svn update on the trunk.
-4. Go to the trunk and run "python preparetest.py dist\install\win\files".
-5. Go to dist\install\win\files and run "python nminit.py".
-6. In most cases, it won't be necessary to change the python\ directory, but if you think you need to (i.e. if we are updating the version of python we use), make sure to check the python_info.txt file first.
-7. Zip the win\ folder back up into win.zip, and distribute!
---
+++ dist/win/install.bat
@@ -0,0 +1,82 @@
+@echo off
+
+:check_os
+REM Check for Vista or XP
+IF "%ALLUSERSPROFILE%"=="%HOMEDRIVE%\ProgramData" GOTO set_vista
+GOTO set_xp
+
+:set_vista
+set PROG_PATH=%HOMEDRIVE%%HOMEPATH%\Documents\seattle_repy
+set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+GOTO :check_seattle_installed
+
+:set_xp
+set PROG_PATH=%HOMEDRIVE%%HOMEPATH%\My Documents\seattle_repy
+set STARTUP_FOLDER=%HOMEDRIVE%%HOMEPATH%\Start Menu\Programs\Startup
+GOTO :check_seattle_installed
+
+:check_seattle_installed
+IF EXIST "%PROG_PATH%" GOTO seattle_installed
+GOTO seattle_not_installed
+
+:seattle_installed
+ECHO seattle is already installed on your computer.
+GOTO failure
+
+:seattle_not_installed
+echo Installing seattle...
+GOTO copy_files
+
+:copy_files
+echo Creating install directory...
+mkdir "%PROG_PATH%"
+echo Done.
+echo Copying python dependencies...
+xcopy "python\*" "%PROG_PATH%\" /E /Y > nul 2> nul
+echo Done.
+echo Copying seattle files...
+xcopy "RUNME.bat" "%PROG_PATH%\" /E /Y > nul 2> nul
+xcopy "files\*" "%PROG_PATH%\" /E /Y > nul 2> nul
+echo Done.
+GOTO add_to_startup
+
+:add_to_startup
+echo Adding seattle to startup...
+echo @echo off > seattle.bat
+echo cd "%PROG_PATH%" >> seattle.bat
+echo start /min seattle.bat >> seattle.bat
+echo exit >> seattle.bat
+copy seattle.bat "%STARTUP_FOLDER%\" > nul
+echo Done.
+GOTO generate_uninstaller
+
+:generate_uninstaller
+echo Generating uninstaller...
+echo @echo off > uninstall.bat
+echo IF EXIST "%TMP%\uninstall.bat" GOTO already_copied >> uninstall.bat
+echo GOTO copy >> uninstall.bat
+echo :copy >> uninstall.bat
+echo copy uninstall.bat "%TMP%\uninstall.bat" ^> nul 2^> nul >> uninstall.bat
+echo start /min "%TMP%\uninstall.bat" >> uninstall.bat
+echo GOTO end >> uninstall.bat
+echo :already_copied >> uninstall.bat
+echo echo del /f /q "%STARTUP_FOLDER%\seattle.bat" ^> nul 2^> nul >> uninstall.bat
+echo rmdir /s /q "%PROG_PATH%" ^> nul 2^> nul >> uninstall.bat
+echo GOTO end >> uninstall.bat
+echo :end >> uninstall.bat
+copy uninstall.bat "%PROG_PATH%\" > nul 2> nul
+echo Done.
+GOTO success
+
+:failure
+echo Installation failed.
+GOTO end
+
+:success
+echo seattle was successfully installed on your computer.
+%HOMEDRIVE%
+cd %PROG_PATH%
+start /min RUNME.bat
+GOTO end
+
+:end
