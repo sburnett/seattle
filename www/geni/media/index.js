81bde844ed3d2dd9de2cb5ee5902d149f77d28b9
peter <peter@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2008-10-19 19:31:25 +0000

    added threshold for hiding tables

--- www/geni/media/index.js
+++ www/geni/media/index.js
@@ -1,6 +1,7 @@
 var count1 = 0;
 var count2 = 0;
 var count3 = 0;
+var hideWhenNumber = 5
 
 window.onload = function() {
 	var tabs = $$(".tab");
@@ -24,45 +25,63 @@ window.onload = function() {
 	try { if(!maximizer3.innerText) maximizer3.innerText = maximizer3.textContent; } catch(e) {}
 	try { if(!maximizer1.innerText) maximizer1.innerText = maximizer1.textContent; } catch(e) {}
 	try { if(!maximizer2.innerText) maximizer2.innerText = maximizer2.textContent; } catch(e) {}
-	$("maximizer1").onclick = table1unhide;
-	$("maximizer2").onclick = table2unhide;
-	$("maximizer3").onclick = table3unhide;
+	if (count1 < hideWhenNumber) {
+		$("maximizer1").onclick = table1unhide;
+	} else {
+		table1unhide();
+	}
+	if (count2 < hideWhenNumber) {
+		$("maximizer2").onclick = table2unhide;
+	} else {
+		table2unhide();
+	}
+	if (count3 < hideWhenNumber) {
+		$("maximizer3").onclick = table3unhide;
+	} else {
+		table3unhide();
+	}
 };
 
 function table3unhide() {
 	$("sharedResources").style.display = "block";
 	$("maximizer3").textContent = count3 + " Shares (click to hide)";
 	try { if(!maximizer3.innerText) maximizer3.innerText = maximizer3.textContent; } catch(e) {}
-	$("maximizer3").onclick = function() {
-		$("sharedResources").style.display = "none";
-		$("maximizer3").textContent = count3 + " Shares (click to expand)";
-		try { if(!maximizer3.innerText) maximizer3.innerText = maximizer3.textContent; } catch(e) {}
-		$("maximizer3").onclick = table3unhide;
-	}
+	$("maximizer3").onclick = table3hide;
+}
+
+function table3hide() {
+	$("sharedResources").style.display = "none";
+	$("maximizer3").textContent = count3 + " Shares (click to expand)";
+	try { if(!maximizer3.innerText) maximizer3.innerText = maximizer3.textContent; } catch(e) {}
+	$("maximizer3").onclick = table3unhide;
 }
 
 function table1unhide() {
 	$("table1").style.display = "block";
 	$("maximizer1").textContent = count1 + " Machines (click to hide)";
 	try { if(!maximizer1.innerText) maximizer1.innerText = maximizer1.textContent; } catch(e) {}
-	$("maximizer1").onclick = function() {
-		$("table1").style.display = "none";
-		$("maximizer1").textContent = count1 + " Machines (click to expand)";
-		try { if(!maximizer1.innerText) maximizer1.innerText = maximizer1.textContent; } catch(e) {}
-		$("maximizer1").onclick = table1unhide;
-	}
+	$("maximizer1").onclick = table1hide;
+}
+
+function table1hide() {
+	$("table1").style.display = "none";
+	$("maximizer1").textContent = count1 + " Machines (click to expand)";
+	try { if(!maximizer1.innerText) maximizer1.innerText = maximizer1.textContent; } catch(e) {}
+	$("maximizer1").onclick = table1unhide;
+}
+
+function table2hide() {
+	$("table2").style.display = "none";
+	$("maximizer2").textContent = count2 + " Machines (click to expand)";
+	try { if(!maximizer2.innerText) maximizer2.innerText = maximizer2.textContent; } catch(e) {}
+	$("maximizer2").onclick = table2unhide;
 }
 
 function table2unhide() {
 	$("table2").style.display = "block";
 	$("maximizer2").textContent = count2 + " Machines (click to hide)";
 	try { if(!maximizer2.innerText) maximizer2.innerText = maximizer2.textContent; } catch(e) {}
-	$("maximizer2").onclick = function() {
-		$("table2").style.display = "none";
-		$("maximizer2").textContent = count2 + " Machines (click to expand)";
-		try { if(!maximizer2.innerText) maximizer2.innerText = maximizer2.textContent; } catch(e) {}
-		$("maximizer2").onclick = table2unhide;
-	}
+	$("maximizer2").onclick = table2hide;
 }
 
 function tally() {
