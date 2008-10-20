ae7bd47367c8c77aa02183be0e9965b9e90343ed
justinc <justinc@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2008-10-20 01:53:36 +0000

    changes to have an easy way to kill the node manager and software updater

---
+++ softwareupdater/seattlestopper.py
@@ -0,0 +1,28 @@
+import runonce
+import os
+import sys
+import nonportable
+
+locklist = [ "seattlenodemanager", "seattlesoftwareupdater" ]
+
+def main():
+
+  # For each locked process, find the PID of the process holding the lock...
+  for lockname in locklist:
+    retval = runonce.getprocesslock(lockname)
+    if retval == True:
+      # I got the lock, it wasn't running...
+      print "The lock '"+lockname+"' was not held"
+      pass
+    elif retval == False:
+      # Someone has the lock, but I can't do anything...
+      print "The lock '"+lockname+"' is not held by an unknown process"
+
+    else:
+      # I know the process ID!   Let's stop the process...
+      print "Stopping the process (pid: "+str(retval)+") with lock '"+lockname+"'"
+      nonportable.portablekill(retval)
+
+
+if __name__ == '__main__':
+  main()
