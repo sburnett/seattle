808f7f2e9587865316b0114895e4b97101264146
ivan <ivan@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2008-10-22 08:27:29 +0000

    final process.php

--- www/customized_installer/process.php
+++ www/customized_installer/process.php
@@ -6,14 +6,14 @@ if (isset($_POST)) {
 		$user = new User($username);
 		
 		if (is_uploaded_file($_FILES["publickey"]["tmp_name"])) {
-			move_uploaded_file($_FILES["publickey"]["tmp_name"], "keys/" . $user->getName() . ".public");
+			move_uploaded_file($_FILES["publickey"]["tmp_name"], "keys/" . $user->getName() . ".publickey");
 			$user->setUploaded(true);
-			$user->readPublicKey("keys/" . $user->getName() . ".public");
-		
+			$user->readPublicKey("keys/" . $user->getName() . ".publickey");
 		} else {
 			$user->setUploaded(false);
-			$user->generateKeys();
-			$user->writeKeys();
+                        $user->generateKeys(); 
+                        // NOTE: generateKeys already writes the key files
+			//$user->writeKeys();
 		}
 		// echo $user->getName() . " is added.";
 	} else if ($_POST['action'] == 'createinstaller') {
@@ -25,16 +25,17 @@ if (isset($_POST)) {
 			}
 			unset($user);
 		}
-		unset($vessel);
+		Unset($vessel);
 		
 		$mypid = getmypid();
-		$vesselinfopy = "/home/ivan/trunk/test/writecustominstallerinfo.py";
-		$prefix = "/var/www/customized_installer/download";
-		exec("mkdir $prefix/$mypid");
-		file_put_contents("$prefix/vesselsinfo_$mypid.txt", outputVesselsInfo($vessels)); 
-		exec("python $vesselinfopy $prefix/vesselsinfo_$mypid.txt $prefix/$mypid/");
-		
-		echo $mypid;
+                $vesselinfopy = "/home/ivan/trunk/test/writecustominstallerinfo.py";
+                $prefix = "/var/www/customized_installer";
+                $dl_prefix = "$prefix/download";
+                exec("rm -Rf $dl_prefix/* && mkdir $dl_prefix/vesselsinfo");
+                file_put_contents("$dl_prefix/vesselsinfo_$mypid.txt", outputVesselsInfo($vessels));
+                exec("python $vesselinfopy $dl_prefix/vesselsinfo_$mypid.txt $dl_prefix/vesselsinfo/");
+                exec("python $prefix/customize_installers.py $dl_prefix/vesselsinfo seattle $dl_prefix/seattle");
+                exec("cp $dl_prefix/seattle_linux.tgz $dl_prefix/seattle_mac.tgz");
 	} else if ($_POST['action'] == 'resetform') {
 		$username = standarize($_POST['username']);
 		if (file_exists(getPublicKeyPath($username)) && !preg_match("/^user_\d$/", $username)) {
@@ -60,7 +61,8 @@ function standarize ($username) {
 }
 
 function getPublicKeyPath ($username) {
-	return "keys/" . $username . ".public";
+        $prefix = "/var/www/customized_installer";
+	return "$prefix/keys/" . $username . ".publickey";
 }
 
 class User {
@@ -91,16 +93,25 @@ class User {
 	}
 	
 	public function generateKeys () {
-		// $keys = explode(" ", exec("keygen.py"));
-		// !!this line needs to be replaced
-		$keys = array("dfajk22f3", "a23f3bc8");
-		$this->public_key = $keys[0];
-		$this->private_key = $keys[1];
+                $prefix = "/var/www/customized_installer";
+                // TODO: make keys directory unique to this session
+                // NOTE: pids do not work here -- we need a **session** persistent unique id
+                // $mypid = getmypid();
+                // exec("mkdir $prefix/keys/$mypid/");
+                
+                // NOTE: this script already creates the public private key files
+		// $keys = explode(" ", exec("python $prefix/generatekeys.py " . $this->name. " 512"));
+                exec("python $prefix/generatekeys.py " . $this->name. " 128");
+                //exec("mv $prefix/" . $this->name . ".publickey $prefix/keys/");
+                //exec("mv $prefix/" . $this->name . ".privatekey $prefix/keys/");
+		// $keys = array("dfajk22f3", "a23f3bc8");
+		//$this->public_key = $keys[0];
+		//$this->private_key = $keys[1];
 	}
 	
 	public function writeKeys () {
-		file_put_contents("keys/" . $this->name . ".public", $this->public_key);
-		file_put_contents("keys/" . $this->name . ".private", $this->private_key);
+		file_put_contents("keys/" . $this->name . ".publickey", $this->public_key);
+		file_put_contents("keys/" . $this->name . ".privatekey", $this->private_key);
 	}
 	
 	public function getPublicKey () {
