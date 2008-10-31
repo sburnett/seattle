55341facf34c6371e057d55783c6bb7bf280837a
sean <sean@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2008-10-31 07:19:01 +0000

    redesigned the installer.html page

--- www/customized_installer/scripts/installer.js
+++ www/customized_installer/scripts/installer.js
@@ -1,12 +1,32 @@
 window.onload = function () {
-	detectOS();
-	
-	
+	var platform = detectOS();
+	switch (platform) {
+		case "win":
+			$("win").addClassName("highlight");
+			break;
+		case "mac":
+			$("mac").addClassName("highlight");
+			break;
+		case "linux":
+			$("linux").addClassName("highlight");
+			break;
+		default:
+			break;
+	}
+//	alert(platform);
 	
 }
 
 function detectOS () {
-	navigator.platform.match(//)
-	
-	
+	var platform;
+	if (navigator.platform.match(/win/i)) {
+		platform = "win";
+	} else if (navigator.platform.match(/mac/i)) {
+		platform = "mac";
+	} else if (navigator.platform.match(/linux/i)) {
+		platform = "linux";
+	} else {
+		platform = "unknown";
+	}
+	return platform;
 }
