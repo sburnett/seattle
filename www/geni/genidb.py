232e2dfa594b685c4a23a0803fe4aab5e0876114
ivan <ivan@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2008-11-03 00:34:51 +0000

    more

--- www/geni/genidb.py
+++ www/geni/genidb.py
@@ -1,27 +1,31 @@
+import traceback
 from settings import *
 #import manage
-from control.models import User,Donation,Vessel,VesselMap,Share,pop_key
+#import control.models as models
+from control.models import User,Donation,Vessel,VesselMap,Share,VesselPorts,pop_key
 from django.db import transaction
+#import django.db.models as models
+import django.core.exceptions as exceptions
 import sys
-
-# 1 day
-EXPIRE_TIME_SECS = 86400
 
 def __get_guser__(donor_pubkey):
     '''
     returns user object based on the donor_pubkey, None on error
     '''
+        
     try:
-        return User.objects.all.get(donor_pubkey=pubkey)
-    except:
+        return User.objects.get(donor_pubkey=donor_pubkey)
+# this is equivalent to:
+#    for u in User.objects.all():
+#        print u.donor_pubkey
+#        print (u.donor_pubkey == donor_pubkey)
+    except exceptions.ObjectDoesNotExist:
         return None
     
 def lookup_node(node_pubkey):
     try:
         node = Donation.objects.get(pubkey=node_pubkey)
-    except:
-        print "Unable to find a donated node in genidb with pubkey: %s"%(node_pubkey)
-        print "Error:", sys.exc_info()[0]
+    except exceptions.ObjectDoesNotExist:
         return None
     return node
 
@@ -58,16 +62,16 @@ def create_update_node(node,node_info,donor_pubkey):
         # build new record
         guser = __get_guser__(donor_pubkey)
         if guser == None:
-            return False
+            raise Exception, "create_update_node: couldn't find donor with key string " + donor_pubkey
     
         node = Donation(user=guser,
-                        pubkey=node_info[''],
+                        pubkey=node_info['nodeid'],
                         ip = node_info['ip'],
                         port = node_info['port'],
                         status = node_info['status'],
                         version = node_info['version'],
-                        owner_pubkey = node_info['owner_pubkey'],
-                        ownerprivkey = node_info['owner_privkey'])
+                        owner_pubkey = node_info['owner_pubkey_str'],
+                        owner_privkey = node_info['owner_privkey_str'])
     else:
         node.version = node_info['version']
         node.ip = node_info['ip']
@@ -75,7 +79,7 @@ def create_update_node(node,node_info,donor_pubkey):
         node.status = node_info['status']
     # save record
     node.save()
-    return True
+    return node
 
 @transaction.commit_manually
 def add_node_vessels(node,newstatus,vlist,vextra):
@@ -87,29 +91,45 @@ def add_node_vessels(node,newstatus,vlist,vextra):
     NOTE: in case of error in this function, use:
     transaction.rollback(); return False;
     '''
-    
-    
-    node.status = newstatus
-    node.save()
-    expire_time = time.time() + EXPIRE_TIME_SECS
-    for v in vlist:
-        vrec = Vessel(donation = node,
-                      expiration = expire_time,
-                      port = v['port'],
-                      name = v['name'],
-                      status = v['status'],
-                      extra_vessel = False)
-        vrec.save()
-        
-    vrec_extra = Vessel(donation = node,
-                        expiration = expire_time,
-                        port = vextra['port'],
-                        name = vextra['name'],
-                        status = vextra['status'],
-                        extra_vessel = True)
-    vrec_extra.save()
-    # success
-    transaction.commit()
+    print "in add_node_vessels"
+    print "newstatus: ", newstatus
+    print "VLIST: ", vlist
+    print "VEXTRA: ", vextra
+
+    try:
+        node.status = newstatus
+        node.save()
+        # create the vessel records
+        for (vname,vports) in vlist:
+            v_rec = Vessel(donation = node,
+                           name = vname,
+                           status = "Initialized",
+                           extra_vessel = False)
+            v_rec.save()
+            # create the vessel->port mapping for all ports assigned to this vessel
+            for vport in vports:
+                vport_rec = VesselPorts(vessel = v_rec, port = vport)
+                vport_rec.save()
+            
+        # handle the extra (leftover) vessel
+        vextra_rec = Vessel(donation = node,
+                            name = vextra[0],
+                            status = 'Intialized',
+                            extra_vessel = True)
+        vextra_rec.save()
+        # create the vessel->port mapping for all ports assigned to this vessel
+        for vport in vextra[1]:
+            vport_rec = VesselPorts(vessel = vextra_rec, port = vport)
+            vport_rec.save()
+            
+    except:
+        traceback.print_exc()
+        transaction.rollback()
+        sys.exit()
+        return False
+    else:
+        # success
+        transaction.commit()
     return True
 
 def update_node(node,newstatus):
@@ -122,6 +142,12 @@ def update_node(node,newstatus):
     node.save()
 
 if __name__ == '__main__':
-    print DATABASE_ENGINE
-    print User.objects.filter(port=9112)
+    owner_key_str = '102581760161525849299480945390773462604452516673727153784436170616998785707093 828932698601553000080805048876997096119480280205665028530494046780633311955857068025674724327917536303134469749169449682860787080843248925875229718762166573070303171801656742580803907000114678479476569028271760705524370153555002217325596530079500410486609590403866179677127524204126433713843094372680813899'
+    donor_privkey = get_donor_privkey(owner_key_str)
+    donor_privkey = get_donor_privkey(owner_key_str+"1")
+    print
+    print "looking for: \n", owner_key_str
+    print donor_privkey
+    #print DATABASE_ENGINE
+    #print User.objects.filter(port=9112)
     #lookup_donor(9112)
