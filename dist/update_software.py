6a8d423609108180fcc18382041c62b81e157f57
butaud <butaud@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2008-12-02 04:50:12 +0000

    Added script to run software updater

---
+++ dist/update_software.py
@@ -0,0 +1,58 @@
+"""
+<Program Name>
+  update_software.py
+
+<Started>
+  December 1, 2008
+
+<Author>
+  Carter Butaud
+
+<Purpose>
+  Populates the folder that the software updater checks with
+  the latest version of the code.
+"""
+
+import sys
+import make_base_installers
+
+UPDATE_SITE = "/home/couvb/public_html/updatesite"
+DEBUG = False
+
+def update(trunk_location):
+  """
+  <Purpose>
+    Populates the update directory (set by a constant) with the
+    program files from the current repository.
+  
+  <Arguments>
+    trunk_location:
+      The location of the repository's trunk directory, used to
+      find the program files.
+    
+  <Exceptions>
+    IOError on bad filepath.
+  
+  <Side Effects>
+    None.
+
+  <Returns>
+    None.
+  """
+  update_dir = UPDATE_SITE + "/real"
+  # If we're in DEBUG mode, don't use the real update
+  # directory
+  if DEBUG:
+      update_dir = UPDATE_SITE + "/test"
+  make_base_installers.prepare_initial_files(trunk_location, update_dir)
+
+def main():
+    if len(sys.argv) < 2:
+        print "usage: python update_software.py trunk/location/ [-d]"
+    else:
+        if sys.argv[2] == "-d":
+            DEBUG = True
+        update(sys.argv[1])
+
+if __name__ == "__main__":
+    main()
