e52a0422b18be45a82b04484d255be74dbcd80c2
sean <sean@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2008-12-02 09:16:42 +0000

    added the data structure for the vesselinfo

--- www/remote_installer/remote_client.py
+++ www/remote_installer/remote_client.py
@@ -1,8 +1,20 @@
 import xmlrpclib
 
-server = xmlrpclib.Server('http://localhost:8000')
-print server.chop_in_half('I am a confidant guy')
-print server.repeat('Repetition is the key to learning!\n', 5)
+server = xmlrpclib.erver('http:S//localhost:8000')
+
+
+vessel_info = 
+	[
+		(2, sean.public, []),
+		(3, ivan.public, [peter.public, justin.public, sean.public]),
+		(1, peter.public, [justin.public, sean.public])
+	]
+
+server.create_installer(vessel_info);
+
+
+# print server.chop_in_half('I am a confidant guy')
+# print server.repeat('Repetition is the key to learning!\n', 5)
 # print server._string('<= underscore')
 # print server.python_string.join(['I', 'like it!'], " don't ")
 # print server._privateFunction() # Will throw an exception
--- www/remote_installer/remote_server.py
+++ www/remote_installer/remote_server.py
@@ -1,30 +1,54 @@
 #XML-RPC Server
 
-import SimpleXMLRPCServer
-import SocketServer
+# Configure below
+LISTEN_HOST = '127.0.0.1' # You should not use '' here, unless you have a real FQDN.
+LISTEN_PORT = 8000
 
-class StringFunctions:
+# Paths
+PREFIX = "/var/www/remote_installer";
+INSTALLER_SCRIPT = PREFIX + "/customize_installers.py";
+VESSEL_INFO = PREFIX + "/writecustominstallerinfo.py";
+
+import os
+import SimpleXMLRPCServer
+
+# importng Justin's code
+# import Carter's code
+# import ... as vesselinfopy
+
+class generateInstaller:
     def __init__(self):
         # Make all of the Python string functions available through
         # python_string.func_name
         import string
         self.python_string = string
 
-    def _privateFunction(self):
-        # This function cannot be called through XML-RPC because it
-        # starts with an '_'
-        pass
-    
-    def chop_in_half(self, astr):
-        return astr[:len(astr)/2]
+    def createInstaller(self):
+	# vesselinfopy.generate(..)
+#		os.system("python $vesselinfopy $dl_prefix/vesselsinfo.txt $dl_prefix/vesselsinfo/");
+#       os.system("cd $dl_prefix/tmp/ && python $carter_script mlw $dl_prefix/vesselsinfo $dl_prefix/ > /tmp/carter.out.php 2> /tmp/carter.err.php");		
+#		os.system("zip -j $dl_prefix/private.zip $dl_prefix/*.privatekey");
+#		os.system("zip -j $dl_prefix/public.zip $dl_prefix/*.publickey");
+#       os.system("cp $dl_prefix/seattle_linux.tgz $dl_prefix/seattle_mac.tgz");
 
-    def repeat(self, astr, times):
-        return astr * times
+
+	def create_installer(self, vessels):
+		""""
+		arguments:
+			vessels of form [(percent1, owner1, [u1, u2, ...]), (perent2, owner2, u1', u2',...),...]
+			where percent is ..
+			owner is
+			u is ..
 	
-
+		"""
+		for (percent, owner, users) in vessels:
+			owner_key = owner
+			user_keys = users
+			for u in users
+				u_key = users[u]
+			
     
-server = SimpleXMLRPCServer.SimpleXMLRPCServer(("localhost", 8000))
-server.register_instance(StringFunctions())
-#server.register_function(lambda astr: '_' + astr, '_string')
+server = SimpleXMLRPCServer.SimpleXMLRPCServer((LISTEN_HOST, LISTEN_PORT))
+server.register_instance(generateInstaller())
 server.serve_forever()
 
