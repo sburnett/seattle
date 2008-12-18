6cfc122a890a3c42901b7bed9ccf15aa65c1f99c
butaud <butaud@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2008-12-18 21:21:58 +0000

    Added check for python to install shell script

--- dist/linux/scripts/install.sh
+++ dist/linux/scripts/install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-
-python install.py
+which_out=`which python`
+if [ "$which_out" = "" ]; then
+    echo seattle requires that python be installed on your computer.
+    echo Please install python and try again.
+else
+    python install.py
+fi
 exit
