f040c672ae325981185d9b17e3d0181a91bd75ba
sean <sean@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2008-12-29 15:18:22 +0000

    Fixed most errors in the code, making it almost work Added comments and documentation strings The installer still doesn't create the installer Key generator doesn't work for some reasons

--- www/remote_installer/remote_client.py
+++ www/remote_installer/remote_client.py
@@ -1,38 +1,122 @@
+"""
+<Program Name>
+	remote_client.py
+
+<Started>
+	November 28th, 2008
+
+<Author>
+	shawiz@cs.washington.edu
+	Xuanhua Ren (Sean)
+
+<Purpose>
+	Client that calls the remote build installer function to create
+	customized installer
+"""
+
 import xmlrpclib
-import generatekeys
 
-
-def __genkey__(user):
+def __genkey__(username):
 	"""
-		arguments:
-			user - the user who we want to generate key for
-		return:
-			the key for the user
+	<Purpose>
+		Internal method to generate keys for users
+	<Arguments>
+		username:
+			the user who we want to generate key for
+	<Returns>
+		the key for the user
 	"""
 	
+	# write keys to the file
+	os.system("python generatekeys.py %s %s %s"%(username, 20, "keys"))
+	
+	# read the public key from file
+	f = open("/keys/%s.public"%(username), 'r')
+	key = f.read()
+	f.close()
+	
+	return key
+
 
 def create_installer_with_keys(user_key_dict, vessel_info):
-	server = xmlrpclib.erver('http://localhost:8000')
-	ret = server.create_installer(user_key_dict, vessel_info)
-	return "Success!"
-	# check errors
-	# return something
+	"""
+	<Purpose>
+		Create the installe with given user-key pairs and vessel info
+	<Arguments>
+		user_key_dict:
+			dictionary of form {"username1": "key1", "username2": "key2", ...}
+		vessel_info:
+			vessels of form [user_key_dict, (percent1, owner1, [u1, u2, ...]),
+			(perent2, owner2, u1', u2',...),...]
+			where user_key_dict is a dictionar mapping usernames to public keys
+			percent is the percent of each vessel..
+			owner is the owner username of each vessel
+			u's are the usernames corresponding to users of each vessel
+	<Returns>
+		the url for the installer
+	"""
+
+	server = xmlrpclib.Server('http://localhost:8000')
+	return server.build_installer(user_key_dict, vessel_info)
 	
-def create_installer_without_keys(vessels):
+
+def create_installer_without_keys(vessel_info):
+	"""
+	<Purpose>
+		Create the installe with vessel info only
+	<Arguments>
+		vessel_info:
+			vessels of form [user_key_dict, (percent1, owner1, [u1, u2, ...]),
+			(perent2, owner2, u1', u2',...),...]
+			where user_key_dict is a dictionar mapping usernames to public keys
+			percent is the percent of each vessel..
+			owner is the owner username of each vessel
+			u's are the usernames corresponding to users of each vessel
+	<Returns>
+		the url for the installer
+	"""
+
 	user_key_dict = {}
-	for v in vessels:
-		user_key_dict[v] = __genkey__(v)
-	create_installer_with_keys(user_key_dict, vessels)
 	
-def create_installer_without_keys_with_equal_vessels(usernames):
-	create_installer_without_keys(vessels)
+	# generate keys for every owner and user
+	for (percent, owner, users) in vessel_info:
+		user_key_dict[owner] = __genkey__(owner)
+		for u in users:
+			user_key_dict[u] = __genkey__(u)
+
+	return create_installer_with_keys(user_key_dict, vessel_info)
+
+
+# varies depends on how many users we have
+# def create_installer_without_keys_with_equal_vessels(usernames):
+#	create_installer_without_keys(vessels)
+
+
+def create_installer_one_user(username):
+	"""
+	<Purpose>
+		Create the installe with only one user
+	<Arguments>
+		username:
+			username of the user
+	<Returns>
+		the url for the installer
+	"""
+
+	user_key_dict = {}
+	user_key_dict[username] = __genkey__(username)
 	
-#def create_installer_one_user(username):
-	#...
+	# create the single-user vessel info
+	vessel_info = [(8, username, [])]
+	
+	return create_installer_with_keys(user_key_dict, vessel_info)
 
-user_keys_dict = {"sean": "5935158103850138518353", "ivan": "902039572093570927590235", "justin": "9328446592099373728274", "peter": "834748293048249020810"}
 
+# default user key dictionary for testing purpose
+user_key_dict = {"sean": "593515810385013851833", "ivan": "902039572093570927235", "justin": "9328446592099373728274", "peter": "834748293048249020810"}
+
+# default vessel info for testing purpose
 vessel_info = [(2, "sean", []), (3, "ivan", ["peter", "justin", "sean"]), (1, "peter", ["justin", "sean"])]
 
 if __name__ == "__main__":
-	print create_installer_with_keys(user_keys_dict, vessel_info)
+	print create_installer_with_keys(user_key_dict, vessel_info)
--- www/remote_installer/remote_server.py
+++ www/remote_installer/remote_server.py
@@ -1,18 +1,28 @@
 """
-Author: Xuanhua Ren
+<Program Name>
+	remote_server.py
 
-Module: Create the customized installer through XML_RPC
+<Started>
+	November 28th, 2008
 
-Start date: November 28th, 2008
+<Author>
+	shawiz@cs.washington.edu
+	Xuanhua Ren (Sean)
 
+<Purpose>
+	Provide remote methods to build customized installer on server side
+	through XML-RPC.
+	
+	The methods are core functions to build the customized installer for
+	the user, and it will eventually the urls for the customized built
+	installers
 
 """
 
 
+# todos: make external calls into importing
 
-#todos: make external calls into importing
-#XML-RPC Server
-
+import sys
 import os
 import re
 import SimpleXMLRPCServer
@@ -22,106 +32,155 @@ import SimpleXMLRPCServer
 # import ... as vesselinfopy
 
 class generateInstaller:
-    def __init__(self, prefix, dl_prefix):
+	"""
+	<Purpose>
+		Class to generate the installer
+	<Example Use>
+		# create gen installer instance
+		gen_installer = generateInstaller(prefix, dl_prefix)
+		# build the installer and get its url
+		url = server.build_installer(user_key_dict, vessel_info)
+	"""
+	
+	def __init__(self, prefix, dl_prefix):
+		"""
+		<Purpose>
+			Initialize the instance
+		<Arguments>
+			prefix:
+				the system prefix of current directory
+			dl_prefix:
+				prefix of download folder
+	   """
 		self.prefix = prefix
 		self.dl_prefix = self.prefix + "/" + dl_prefix
 		self.installer_script = self.prefix + "/customize_installers.py"
 		self.vesselinfo_script = self.prefix + "/writecustominstallerinfo.py"
 
-	def standarize_name(self, username):
-		"""
-		arguments:
-			username that may contain white spaces or uppercase letters
-		return:
-			username with all lowercase and separated by underscores
-		"""
-		return re.sub("\s+", "_", string.lower(username))
 
+	# def standarize_name(self, username):
+	#	 """
+	#	arguments:
+	#		username that may contain white spaces or uppercase letters
+	#	return:
+	#		username with all lowercase and separated by underscores
+	#	"""
+	#	return re.sub("\s+", "_", string.lower(username))
+   
 
 	def output_vessel_info(self, user_key_dict, vessels):
 		"""
-		arguments:
-			vessels of form [user_key_dict, (percent1, owner1, [u1, u2, ...]), (perent2, owner2, u1', u2',...),...]
-			where user_key_dict is a dictionar mapping usernames to public keys
-			percent is the percent of each vessel..
-			owner is the owner username of each vessel
-			u's are the usernames corresponding to users of each vessel
-		return:
-			output in format of 
+		<Purpose>
+			Output the vessel information in a specific format
+		<Arguments>
+			user_key_dict:
+				dictionary of form {"username1": "key1", "username2": "key2", ...}
+			vessels:
+				vessels of form [user_key_dict, (percent1, owner1, [u1, u2, ...]),
+				(perent2, owner2, u1', u2',...),...]
+				where user_key_dict is a dictionar mapping usernames to public keys
+				percent is the percent of each vessel..
+				owner is the owner username of each vessel
+				u's are the usernames corresponding to users of each vessel
+		<Returns>
+			output string in format of 
 				Percent xxx
 				Owner xxx
 				User xxx
 				User xxx
 				...
 		"""
-		
-		output = "";
+
+		output = ""
 		for (percent, owner, users) in vessels:
-			output += "Percent " + percent + "\n"
-			output += "Owner " + user_key_dict[owner] + "\n"	
-			for u in users
+			output += "Percent " + str(percent) + "\n"
+			output += "Owner " + user_key_dict[owner] + "\n"	   
+			for u in users:
 				output += "User " + user_key_dict[u] + "\n"
- 		return output
+		return output
 
-	def create_installer(self, user_key_dict, vessel_info):
-		
-		
-	def build_installer(self, username, dist_char, user_key_dict, vessels):
-	    '''
-	    returns url to the finished installer
-	    dist_char is in "lwm"
-	    '''
 
-	    # prefix dir is specific to this user		
-	    user_prefix = self.prefix + "/%s_dist"%(username)
-	    
+	def build_installer(self, user_key_dict, vessels):
+		"""
+		<Purpose>
+			Create the customized installer by taking the user-key pair and
+			vessel info
+				
+		<Arguments>
+			user_key_dict:
+				the dictionary containing the user - public key pair
+				see method above for detailed format
+			vessels:
+				the vessel information given by the user
+				see method above for detailed format
+			
+		<Returns>
+			The url of the installer it builds
+		"""
+		
+		# a dummy username for testing (needs to be removed)
+		username = "foo"
+		# a dummy dist_char for testing (needs to be removed)
+		dist_char = "m"
+		
+		# prefix dir is specific to this user
+		user_prefix = self.prefix + "/%s_dist"%(username)
+
 		# remove and recreate the prefix dir
-	    os.system("rm -Rf %s/"%(user_prefix))
-	    os.system("mkdir %s/"%(user_prefix))
+		os.system("rm -Rf %s/"%(user_prefix))
+		os.system("mkdir %s/"%(user_prefix))
 
-	    # write out to file the user's donor key
-	    f = open('%s/%s'%(user_prefix, username),'w');
-	    f.write("%s"%(geni_user.donor_pubkey))
-	    f.close()
+		# write out to file the user's donor key
+		# f = open('%s/%s'%(user_prefix, username),'w');
+		# f.write("%s"%(username.donor_pubkey))
+		# f.close()
 
-	    # write out to file the geni lookup key
-	    f = open('%s/%s_geni'%(user_prefix, username),'w');
-	    f.write("%s"%(genilookuppubkey))
-	    f.close()
+		# write out to file the geni lookup key
+		# f = open('%s/%s_geni'%(user_prefix, username),'w');
+		# f.write("%s"%(genilookuppubkey))
+		# f.close()
 
-	    # write out to file the vesselinfo to customize the installer
-	    # vesselinfo = '''Percent 8\nOwner %s/%s\nUser %s/%s_geni\n'''%(user_prefix, username, user_prefix, username);
-		vessel_info = output_vessel_info(user_key_dict, vessels)
-	    # f = open('%s/vesselinfo'%(user_prefix),'w');
-	    # f.write("%s"%(vesselinfo))
-	    # f.close()
+		# write out to file the vesselinfo to customize the installer
+		vessel_info = self.output_vessel_info(user_key_dict, vessels)
+		f = open('%s/vesselinfo'%(user_prefix),'w');
+		f.write("%s"%(vessel_info))
+		f.close()
 
-	    #vesselinfo_script = prefix + "writecustominstallerinfo.py"
-	    #installer_script = prefix + "customize_installers.py"
+		# create the dir where vesselinfo will be created
+		os.system("mkdir %s/vesselinfodir/"%(self.prefix))
+		
+		# create the vessel info
+		cmd = "cd %s && python %s %s/vesselinfo %s/vesselinfodir 2> /tmp/customize.err > /tmp/customize.out"%(self.prefix, self.vesselinfo_script, user_prefix, self.prefix)
+		#f = open("/tmp/out", "w")
+		#f.write(cmd)
+		os.system(cmd)
+		
+		# run carter's script to create the installer of the particular type ((w)in, (l)inux, or (m)ac)
+		os.system("python %s %s %s/vesselinfodir/ %s/ > /tmp/installer.out 2> /tmp/installer.err"%(self.installer_script, dist_char, self.prefix, self.prefix))
+		#os.system("python %s %s %s/vesselinfodir/ %s/ &> /tmp/out"%(carter_script, dist_char, prefix,prefix))
+		
+		# compose and return the url to which the user needs to be redirected
+		return "http://seattle.cs.washington.edu/dist/geni/%s_dist/"%(username)	   
 
-	    # create the dir where vesselinfo will be created
-	    os.system("mkdir %s/vesselinfodir/"%(prefix))
-	    # create the vessel info
-	    cmd = "cd %s && python %s %s/vesselinfo %s/vesselinfodir 2> /tmp/customize.err > /tmp/customize.out"%(prefix, self.vesselinfo_script, prefix, prefix)
-	    #f = open("/tmp/out", "w")
-	    #f.write(cmd)
-	    os.system(cmd)
-	    # run carter's script to create the installer of the particular type ((w)in, (l)inux, or (m)ac)
-	    os.system("python %s %s %s/vesselinfodir/ %s/ > /tmp/installer.out 2> /tmp/installer.err"%(self.installer_script, dist_char, prefix, prefix))
-	    #os.system("python %s %s %s/vesselinfodir/ %s/ &> /tmp/out"%(carter_script, dist_char, prefix,prefix))
-	    # compose and return the url to which the user needs to be redirected
-	    # redir_url = "http://seattle.cs.washington.edu/dist/geni/%s_dist/"%(username)
-	    return True
+
+
 
 
 if __name__ == "__main__":
 
-	# get args from user here
-	
-	host = '127.0.0.1'	# You should not use '' here, unless you have a real FQDN.
+	if len(sys.argv) < 4:
+		print "usage: python remote_server.py hostname port download_prefix"
+	else:
+		host = sys.argv[1]
+		port = sys.argv[2]
+		dl_prefix = sys.argv[3]
+
+	# default arguments for testing purpose
+	host = '127.0.0.1'
 	port = 8000
 	prefix = "/Users/shawiz/Development/research/www/remote_installer"
 	dl_prefix = "download"
+	
 	
 	# create the XMLRPC server
 	server = SimpleXMLRPCServer.SimpleXMLRPCServer((host, port))
@@ -131,4 +190,3 @@ if __name__ == "__main__":
 	server.register_instance(gen_installer)
 	# begin the serving loop
 	server.serve_forever()
-
