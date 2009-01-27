87938ec39088177a3160fc3fe5ee545fcd81e34a
kimbrl <kimbrl@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-01-27 00:44:42 +0000

    Added thread safety to forwarder.py, this needs further testing to ensure there are no concurency issues.

--- natlayer/forwarder.py
+++ natlayer/forwarder.py
@@ -16,23 +16,37 @@ client = {} # Tracks connected clients
 server = {} # Tracks connected servers
 
 # Checks all the sockets for data, and forwards everything
-# TODO: make this (thread-)safe, e.g. iterating over dictionaries while running
+# Im considering making this method into two seperate threads
+# one for checking server sockets, and another for the client
+# sockets.  
 def run():
-  # Start an event for each server socket
-  for srv in server:
-    if not srv in events:
-      #print "Sampling Server ", srv
-      events[srv] = settimer(0,checkServerSocket,[srv])
   
-  # Start an event for each client socket
-  for clt in client:
-    if not clt in eventsClt:
-      #print "Sampling Client ", clt
-      eventsClt[clt] = settimer(0,checkClientSocket,[clt])
-      
+  while True:
+    
+    # Start an event for each server socket
+    mycontext['srv-lock'].acquire()
+    for srv in server:
+      if not srv in events:
+        #print "Sampling Server ", srv
+        events[srv] = settimer(0,checkServerSocket,[srv,server[srv]])
+    mycontext['srv-lock'].release()
+
+    # Start an event for each client socket
+    mycontext['cl-lock'].acquire()
+    for clt in client:
+      if not clt in eventsClt:
+        #print "Sampling Client ", clt
+        eventsClt[clt] = settimer(0,checkClientSocket,[clt,client[clt]])
+    mycontext['cl-lock'].release()
+    
+    sleep(SAMPLE_TIME)
+
+
 # Checks a client socket for data
-def checkClientSocket(clt):
-  cltinfo = client[clt]
+  # Armon, i changed the arguments so this method does not have to read
+  # the client data structure and -> does not wait for a lock to start
+def checkClientSocket(clt,cltinfo):
+  #cltinfo = client[clt]
   data = cltinfo["socket"].recv(DATA_CHUNK)
   
   # Is there any data?
@@ -44,15 +58,18 @@ def checkClientSocket(clt):
     server[cltinfo["server"]].send(frame.toString())
 
   # Clean-up
-  # TODO: make this thread safe
+  mycontext['cl-lock'].acquire()
   if clt in eventsClt:
     del eventsClt[clt]
+  mycontext['cl-lock'].release()
     
+
 # Checks a server socket for data
 # TODO: handle other server requests, like CONN_TERM and CONN_BUF_SIZE
-def checkServerSocket(srv):
+  # Armon, i changed the arguments so this method does not have to read
+  # the server data structure and -> does not wait for a lock to start
+def checkServerSocket(srv,sock):
   # Get data from the server socket as a frame
-  sock = server[srv]
   frame = NATFrame()
   frame.initFromSocket(sock)
   
@@ -63,9 +80,10 @@ def checkServerSocket(srv):
     clientSock.send(frame.frameContent)
 
   # Clean-up
-  # TODO: make this thread safe
+  mycontext['srv-lock'].acquire()
   if srv in events:
     del events[srv]
+  mycontext['srv-lock'].release()
 
 
 # Handles a new connection to the forwarder
@@ -73,37 +91,52 @@ def checkServerSocket(srv):
 def handleit(socket, frame):
   # Setup server, and acknowledge it
   if frame.frameMesgType == INIT_SERVER:
+    mycontext['srv-lock'].acquire()
     server[frame.frameMACAddress] = socket
     resp = NATFrame()
     resp.initAsForwarderResponse(STATUS_CONFIRMED)
     socket.send(resp.toString())
-  
+    mycontext['srv-lock'].release()
+
   # Setup client, and acknowledge
   # TODO: keep track of number of client -> server connections  and implement
   # STATUS_BSY_SERVER
   if frame.frameMesgType == INIT_CLIENT:
     serverMac = frame.frameContent
-    if serverMac in server:
+    mycontext['srv-lock'].acquire()
+    found_server = (serverMac in server)
+    mycontext['srv-lock'].release()
+    if found_server:
+      mycontext['cl-lock'].acquire()
       client[frame.frameMACAddress] = {"socket":socket,"server":frame.frameContent}
       resp = NATFrame()
       resp.initAsForwarderResponse(STATUS_CONFIRMED)
       socket.send(resp.toString())
+      mycontext['cl-lock'].release()
+
     else: # Server is not available on forwarder, error.
       resp = NATFrame()
       resp.initAsForwarderResponse(STATUS_NO_SERVER)
       socket.send(resp.toString())
       socket.close()
 
+
+
+
+
 # Does everything    
 if callfunc == "initialize":
+ 
+  #setup locks for client and server data structures  
+  mycontext['srv-lock'] =getlock()
+  mycontext['cl-lock'] =getlock()
+  
   # Initialize the NAT channel
   natcon = NATConnection(FORWARDER_MAC, callargs[0], int(callargs[1]))
-  
+  #natcon = NATConnection(FORWARDER_MAC,"127.0.0.1" , 12345)
+
   # Setup our process to handle everything
   natcon.frameHandler = handleit
   
   # Check all the sockets once in a while
-  # TODO: Use threads?
-  while True:
-    run()  
-    sleep(SAMPLE_TIME)
+  settimer(0,run,[])
