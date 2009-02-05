c4a7046a417a26ed151c731e64e0af531d32cf91
armon <armon@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-02-05 23:23:13 +0000

    Minor changes for server_waitforconn to have helpful output, and an explicit buffer size.

--- natlayer/server_waitforconn.py
+++ natlayer/server_waitforconn.py
@@ -4,6 +4,7 @@ include NATLayer.py
 # Takes 1 argument, the server's MAC
 
 def newClient(remotemac, socketlikeobj, thisnatcon):
+  print "Connected Client: ",remotemac
   while True:
     data = socketlikeobj.recv(1024)
     if data != "":
@@ -14,7 +15,7 @@ if callfunc == "initialize":
   mac = callargs[0]
   print "Connecting..."
   natcon = NATConnection(mac, "127.0.0.1", 12345)
-  natcon.initServerConnection()
+  natcon.initServerConnection(1024) # 1024 byte buffer
   print "Connected! Starting echo."
   
   natcon.waitforconn(newClient)
