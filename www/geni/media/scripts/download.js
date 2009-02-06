045aa6345bfe696b352370ef6397c9a91af145e2
sean <sean@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-02-06 01:51:15 +0000

    make os detection work

--- www/geni/media/scripts/download.js
+++ www/geni/media/scripts/download.js
@@ -4,5 +4,17 @@ $(document).ready(function() {
 
 function detectOs() {
 	var os = $.browser.OS();
-	// alert(os);
+	switch (os) {
+		case "Win":
+			$("#downloads").prepend($("#win"));
+			break;
+		case "Mac":
+			$("#downloads").prepend($("#mac"));
+			break;
+		case "Linux":
+			$("#downloads").prepend($("#linux"));
+			break;
+		default:
+			break;
+	}
 }
