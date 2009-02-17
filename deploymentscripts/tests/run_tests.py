d1dbc030619b98fca5d842a854cec8c49c1cd267
sal <sal@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-02-17 22:25:12 +0000

    added log folder existence test

--- deploymentscripts/tests/run_tests.py
+++ deploymentscripts/tests/run_tests.py
@@ -16,11 +16,18 @@
 
 import subprocess
 import testsDeployRun
+import os
 
+
+badArgsError="Invalid arguments - usage: python testsDeployRun.py [iplist_file]"
 
 #check general wrong arguments error
 output=subprocess.Popen('python testsDeployRun.py',shell=True,stdout=subprocess.PIPE)
-if output.stdout.read().strip()!="Invalid arguments - usage: python testsDeployRun.py [iplist_file]":
+if output.stdout.read().strip()!=badArgsError:
   print "got unexpected output (for invalid arguments)"
+
+#check if provided is a folder
+if (os.path.isdir("logs"))==False:
+  print "logs folder should exist"
 
 
