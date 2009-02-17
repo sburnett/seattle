6f8ad160c4c89ab36b1dce3b2da9caa35d96b7b5
armon <armon@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-02-17 01:34:11 +0000

    Added multiplexer directory

---
+++ multiplexer/build.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# Builds the tests
+
+# Make the built directory if necessary
+mkdir ./built/
+
+# Remove everything in the build dir
+echo "Removing old built files..."
+rm -Rf ./built/*
+
+# Recreate the folders we need
+mkdir ./built/log/
+mkdir ./built/run/
+
+# Copy resource files
+echo "Copying Resource files..."
+cp ./resource/* ./built/
+
+# Copy the scripts directory
+echo "Copying Script files..."
+cp -R ./scripts ./built/
+
+# Go into the source
+cd src
+
+# Pre-process each file
+echo "Building src files..."
+echo "#####"
+all_files=`ls *.py`
+
+# Copy each file
+for f in ${all_files}
+do
+  echo "Copying: ${f}"
+  cp ${f} ../built/${f} # Copy original file
+done
+
+# Go into built
+cd ../built
+
+# Link to repy and repypp
+ln -s ../../repy/repy.py repy.py
+ln -s ../../seattlelib/repypp.py repypp.py
+
+# Then process
+for f in ${all_files}
+do
+  echo "Pre-processing: ${f}"
+  python repypp.py ${f} ${f}.out # Process file
+  rm ${f}  # Remove non-preprocessed
+  mv ${f}.out ${f} # Replace it with the processed file
+done
+
+# Go into the source
+cd ../tests
+
+# Pre-process each test file
+echo "#####"
+echo "Building test files..."
+echo "#####"
+all_files=`ls *.py`
+for f in ${all_files}
+do
+  echo "Pre-processing: ${f}"
+  cp ${f} ../built/${f}_pre # Copy original file
+  cd ../built
+  # Pre-process the file
+  python repypp.py ${f}_pre ${f}
+  rm ${f}_pre # Remove the original
+  cd ../tests
+done
+
+echo "#####"
+echo "Done"
---
+++ multiplexer/run_tests.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+#
+# Runs the tests
+# Takes 1 optional parameter: class
+# If class parameter is given, then only test_CLASS_*.py will be run
+
+# If we get a SIGINT, do we terminate the test(1) or exit(0)
+SIGINTACT="1"
+
+# Should we block stderr? This prevents ugly error messages when SIGINT is received
+BLOCKERR="1"
+
+function stop_forwarder() {
+	# stop the forwarder
+	echo "Stopping forwarder!"
+	./scripts/stop_forwarder.sh >/dev/null 2>&1
+}
+
+# If the script is interrupted (Control-C) handle this cleanly
+function interrupt() {
+	# Terminate the current test
+	printf "[ SIGINT ]\n"
+	if [ $SIGINTACT -eq "1" ]
+	then
+		kill $CURPID 2>&1 >/dev/null
+	else
+		echo "#####"
+		echo "Caught User Interrupt!"
+		stop_forwarder
+		exit	
+	fi
+}
+
+# Give the user a heads up as ot the expected behavior
+if [ $SIGINTACT -eq "1" ]
+then
+  echo "INFO: SIGINT ( Control-C ) will terminate the currently executing test"
+else
+  echo "INFO: SIGINT ( Control-C ) will stop the execution of run_tests.sh"
+fi
+
+# We don't want output from sub-shells
+if [ $BLOCKERR -eq "1" ]
+then
+  exec 2>/dev/null
+fi
+
+# The PID of the current test
+CURPID=0
+
+# Go into the built dir
+cd built
+
+# Clean old logs
+LOGCOUNT=`ls log/ | grep -c ""`
+if [ ${LOGCOUNT} -gt "0" ]
+then
+  echo "Archiving old logs..."
+  DATE=`date +%s`
+  NAME=logs.${DATE}.tar
+  tar -cf ${NAME} log/*
+  echo "Created Archive: ${NAME}"
+  echo "Clearing old logs..."
+  rm -f ./log/*
+fi
+
+# Stop the old forwarder if it is running
+if [ -f run/forwarder.pid ]
+then
+  echo "Stopping old forwarder..."
+  ./scripts/stop_forwarder.sh
+fi
+
+echo "Starting forwarder..."
+./scripts/start_forwarder.sh
+
+# Setup interrupt handler
+trap "interrupt" SIGINT
+
+# Sleep for a second, give the forwarder a change to start
+echo "Waiting for forwarder to initialize..."
+sleep 5
+
+forwarderpid=`cat ./run/forwarder.pid`
+
+# Check if the forwarder is still running
+forwarderstat=`ps -p ${forwarderpid} | grep -c ""`
+if [ ${forwarderstat} -eq "1" ]
+then
+      echo "Fatal Error! Forwarder has not started!"
+      rm ./run/forwarder.pid
+      exit
+fi
+
+# Pre-process each file
+echo "Running tests..."
+echo "#####"
+
+# Run only selected test "class" if there is a parameter
+if [ $# -gt "0" ]
+then
+  all_tests=`ls test_$1_*.py`
+else
+  all_tests=`ls test_*.py`
+fi
+
+for f in ${all_tests}
+do
+  printf "Running %-60s" ${f}
+  
+  # Log the output
+  run="python repy.py --logfile log/${f}.log restrictions.default ${f}"
+  ${run} & >/dev/null 2>&1
+  
+  # Save the current PID
+  CURPID=$!
+
+  # Wait for that to finish 
+  wait >/dev/null 2>&1
+
+  # Get exit code
+  EXITCODE=$?
+  
+  # Only print if the process has a clean exit code
+  # This is so SIGINT will print [ SIGINT ]
+  if [ $EXITCODE = 0 ]
+  then
+    # Check if the log size is 0
+    size=`cat ./log/${f}.log.old | grep -c ''`
+    if [ ${size} -eq "0" ]
+    then
+      printf "  [ PASSED ]\n"
+    else
+      printf "  [ FAILED ]\n"
+    fi
+  fi
+
+  # Sleep for a second
+  sleep 1
+  
+  # Check if the forwarder is still running
+  forwarderstat=`ps -p ${forwarderpid} | grep -c ""`
+  if [ ${forwarderstat} -eq "1" ]
+  then
+      echo "Fatal Error! Forwarder Dead!"
+      rm ./run/forwarder.pid
+      exit
+  fi
+done
+
+echo "#####"
+echo "Done!"
+
+# Stop the forwarder
+stop_forwarder
