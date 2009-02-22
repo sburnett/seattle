949bf080288a7dcef1efdee7f1353c73c0a38186
butaud <butaud@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-02-22 06:16:04 +0000

    Made more abstractions in py_BandwidthClient.py

--- resource/py_BandwidthClient.py
+++ resource/py_BandwidthClient.py
@@ -24,32 +24,30 @@ def get_mess(size, idstring, index):
 def do_nothing(ip, port, mess, ch):
   pass
   
-def get_bandwidth(server_ip):
-  ip = getmyip()
-  mycontext["tcp_port"] = 12345
-  mycontext["udp_port"] = 12346
-  mycontext["server_ip"] = server_ip
+def get_bandwidth(server_ip, server_port, packet_size, packet_count):
+  local_ip = getmyip()
+  local_port = 12345
 
-  num_to_send = 30 # number of packets to be sent
+  num_to_send = packet_size # number of packets to be sent
   
   # Open a tcp connection to the server
-  server_conn = openconn(mycontext["server_ip"], mycontext["tcp_port"])
+  server_conn = openconn(server_ip, server_port)
   server_conn.send(str(num_to_send))
   server_conn.close()
     
   # Listening on the channel we plan to send on increases the send speed
-  connhandle = recvmess(ip, mycontext["udp_port"], do_nothing)
+  connhandle = recvmess(local_ip, local_port, do_nothing)
 
   # Send the UDP packet train
   for i in range(num_to_send):
-    mess = get_mess(512, "testing", i)
-    sendmess(mycontext["server_ip"], mycontext["udp_port"], mess, ip, mycontext["udp_port"])
+    mess = get_mess(packet_size, "testing", i)
+    sendmess(server_ip, server_port, mess, local_ip, local_port)
  
   sleep(1)
   
   # Open the final tcp connection with the server to
   # transmit closing string and recieve test results.
-  server_conn = openconn(mycontext["server_ip"], mycontext["tcp_port"])
+  server_conn = openconn(server_ip, server_port)
   server_conn.send("Done.")
   
   data = server_conn.recv(200)
@@ -61,6 +59,6 @@ if __name__ == "__main__":
   if len(callargs) < 1:
     print "Invalid usage: a server ip is required."
   else:
-    print get_bandwidth(callargs[0])
+    print get_bandwidth(callargs[0], 12345, 512, 300)
 
  
