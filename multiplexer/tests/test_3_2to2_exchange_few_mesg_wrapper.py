ab3681b1b95718e8aaac599f1b01518689a07648
armon <armon@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-02-27 02:19:57 +0000

    Cleaned up test_3_2to2 for the Multiplexer.

--- multiplexer/tests/test_3_2to2_exchange_few_mesg_wrapper.py
+++ multiplexer/tests/test_3_2to2_exchange_few_mesg_wrapper.py
@@ -6,6 +6,7 @@
 include Multiplexer.py
 
 MAX_NUM = 100
+printlock = getlock()
 
 # Handle a new virtual connection
 def new_virtual_conn(remoteip, remoteport, virtualsock, junk, multiplexer):
@@ -18,12 +19,22 @@ def new_virtual_conn(remoteip, remoteport, virtualsock, junk, multiplexer):
       virtualsock.send(str(num))
       num = num + 1
     else:
-      print "Unexpected number! Expected: ", str(num), " Received: ",data
+      printlock.acquire()
+      print "Serv. Unexpected number! Expected: ", str(num), " Received: ",data
+      printlock.release()
 
+  # Sleep a second before closing the socket
+  sleep(1)
+  virtualsock.close()
 
 def client_exchange(clientn,port,local):
   # Try to connect to the other multiplexer
-  virtualsock = mux_openconn("127.0.0.1", port,"127.0.0.1",local)
+  try:
+    virtualsock = mux_openconn("127.0.0.1", port,"127.0.0.1",local)
+  except Exception, e:
+    print e
+    print "Failed to open virtual socket! Client:",clientn,"Port:",port,"From:",local
+    exitall()
 
   # Try to exchange 1 to 10
   num = 1
@@ -34,7 +45,11 @@ def client_exchange(clientn,port,local):
     if data == str(num):
       num = num + 1
     else:
-      print "Unexpected number! Expected: ", str(num), " Received: ",data
+      printlock.acquire()
+      print "Client.",clientn,"Unexpected number! Expected: ", str(num), " Received: ",data
+      printlock.release()
+  
+  virtualsock.close()
   
   # This finished...
   mycontext[clientn] = True
@@ -43,8 +58,8 @@ def timeout():
   print "Reached timeout!"
   exitall()
 
-# Kill us in 25 seconds
-settimer(25, timeout,())
+# Kill us in 30 seconds
+settimer(30, timeout,())
 
 # Setup a waitforconn
 mux_waitforconn("127.0.0.1", 12345, new_virtual_conn)
@@ -52,7 +67,8 @@ mux_waitforconn("127.0.0.1", 12345, new_virtual_conn)
 # Trigger two clients
 settimer(0,client_exchange,[1,12345,20000])
 
-sleep(2)
+# Wait before calling waitforconn
+sleep(5)
 
 # Setup second waitforconn
 mux_waitforconn("127.0.0.1", 20000, new_virtual_conn)
@@ -61,7 +77,10 @@ mux_waitforconn("127.0.0.1", 20000, new_virtual_conn)
 settimer(1,client_exchange,[2,20000,None])
 
 # Wait for a few seconds
-sleep(15)
+sleep(20)
+
+# Get the lock incase we need to print
+printlock.acquire()
 
 # Check that they all registered and finished
 if not 1 in mycontext or not mycontext[1]:
