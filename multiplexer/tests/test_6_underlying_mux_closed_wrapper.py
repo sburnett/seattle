8719c4d199bf8c8fd910d3f665be5d8ebaa80bfd
armon <armon@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-03-04 03:22:33 +0000

    Ticket #149 and #176. Added unit test for when the multiplexer is closed with clients.

---
+++ multiplexer/tests/test_6_underlying_mux_closed_wrapper.py
@@ -0,0 +1,112 @@
+# This test tries to use mux_waitforconn and mux_openconn. It has 3 clients connecting to 1 server.
+# After the first client connects, the server severs the underlying socket.
+
+# Get the Multiplexer
+include Multiplexer.py
+
+MAX_NUM = 50
+
+# Handle a new virtual connection
+def new_virtual_conn(remoteip, remoteport, virtualsock, junk, multiplexer):
+  mycontext["client"] += 1
+  
+  # Close the link for the first client
+  if mycontext["client"] == 1:
+    multiplexer.close()
+    
+  # Exchange 1 to 100
+  try:
+    num = 1
+    while True and num < MAX_NUM:
+      data = virtualsock.recv(1024)
+      if data == str(num):
+        num = num + 1
+        virtualsock.send(str(num))
+        num = num + 1
+      else:
+        print "Unexpected number! Expected: ", str(num), " Received: ",data
+
+    # Sleep a second before closing the socket
+    sleep(1)
+    virtualsock.close()
+  except EnvironmentError, err:
+    if mycontext["client"] == 1 and str(err) == "The socket has been closed!":
+      # This is expected
+      pass
+    else:
+      print "Server Unexpected err:",err
+      raise err
+      exitall()
+
+def client_exchange(clientn):
+  try:
+    # Try to connect to the other multiplexer
+    virtualsock = mux_openconn("127.0.0.1", 12345,timeout=5)
+
+    # Try to exchange 1 to 10
+    num = 1
+    while True and num <= MAX_NUM:
+      virtualsock.send(str(num))
+      num = num + 1
+      data = virtualsock.recv(1024)
+      if data == str(num):
+        num = num + 1
+      else:
+        print "Unexpected number! Expected: ", str(num), " Received: ",data
+  
+    virtualsock.close()
+  
+    # This finished...
+    mycontext[clientn] = True
+    
+  except EnvironmentError, err:
+    if clientn == 1 and str(err) == "The socket has been closed!":
+      # This is expected
+      pass
+    elif clientn == 2 and str(err) == "Connection Refused!":
+      # This is expected
+      pass
+      
+  except Exception, err:
+    print "Client Unexpected Error",clientn,err
+    raise err
+    exitall()  
+
+def timeout():
+  print "Reached timeout!"
+  exitall()
+
+if callfunc=='initialize':
+  # Client num
+  mycontext["client"] = 0
+  
+  # Kill us in 25 seconds
+  settimer(15, timeout,())
+
+  # Setup a waitforconn on a real socket
+  mux_waitforconn("127.0.0.1", 12345, new_virtual_conn)
+
+  # Trigger three clients
+  settimer(0,client_exchange,[1])
+
+  settimer(2,client_exchange,[2])
+
+  settimer(4,client_exchange,[3])
+
+  # Wait for a few seconds
+  sleep(10)
+
+  # Check that they all registered and finished
+  if 1 in mycontext and mycontext[1]:
+    print "Client 1 finished after a closed socket!"
+
+  if 1 in mycontext and mycontext[1]:
+    print "Client 2 finished after a closed socket!"
+
+  if not 3 in mycontext or not mycontext[3]:
+    print "Client 3 failed to finish!"
+  
+  if not mycontext["client"] == 2:
+    print mycontext["client"],"Clients were able to connect. Only 2 expected."
+  
+  exitall()
