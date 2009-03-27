ef7ecf2c272d28d93896902d5199deb51ca6e736
kimbrl <kimbrl@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-03-27 04:25:52 +0000

    added a test to verify the forwarder correctly limits number of servers connected.

---
+++ natlayer/tests/test_v1_5_MAX_SERVERS.py
@@ -0,0 +1,78 @@
+include NATLayer_rpc.py
+
+# This test connects the maximum number of servers to a forwarder.
+# It then tests...
+#   1. additional servers are rejected
+#   2. if a server disconects a new server can connect
+
+
+# There is no expected output
+
+serverMac =  "SERVER-----"
+serverMac2 = "SERVER----2"
+MAX_CONNECTED = 8
+
+# The test will be forced to exit after this many seconds
+# This is necessary since client 3 is expected to block indefinately
+TIME_LIMIT = 30
+
+def new_client(remoteip, remoteport, socketlikeobj, commhandle, thisnatcon):
+ 
+  #clients dont send so this should just block
+  mesg = socketlikeobj.recv(1024)
+  raise Exception, 'Unexpected message recieved by server: '+mesg
+          
+ 
+
+
+# This is called if the test is taking too long
+def long_execution():
+  print "Execution exceeded "+str(TIME_LIMIT)+" seconds!"
+  exitall()
+  
+
+
+
+# run the test!
+if callfunc == "initialize":
+  
+  #mycontext['forwarderip'] = '128.208.1.138'  # attu
+  mycontext['forwarderip']= getmyip()       #local
+
+  # a list of clients
+  server_list = []
+
+
+  # do open conns to fake connections for up to max servers
+  for i in range(MAX_CONNECTED):
+    sleep(.5)
+    server_list.append(openconn(mycontext['forwarderip'],12345))
+     
+  
+  try:
+    # Create server connection, force local forwarder
+    whandle = nat_waitforconn(serverMac, 10000, new_client, 
+                              mycontext['forwarderip'], 12345) 
+  
+  except Exception,e:
+    error = e # this should occur, so do nothing
+  else:
+    print 'No exception thrown when extra servers were connected'
+
+
+  # close a connection, wait for the forwarder check interval to pass
+  # and then try to make one more connection
+  server_list[0].close()
+  
+  sleep(20)  # sleep while waiting for the server to open new connections
+ 
+  
+  # Create server connection, force local forwarder
+  whandle = nat_waitforconn(serverMac2, 10000, new_client, 
+                              mycontext['forwarderip'], 12345) 
+  
+
+  for sock in server_list:
+    sock.close()
+
+  exitall()
