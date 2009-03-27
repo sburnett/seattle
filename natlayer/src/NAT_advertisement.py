478110459902c5fae8d3f2c18fffaf09741b1287
armon <armon@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-03-27 20:01:13 +0000

    Added first version of new advertisement layer.

---
+++ natlayer/src/NAT_advertisement.py
@@ -0,0 +1,126 @@
+"""
+Author: Armon Dadgar
+
+Start date: March 27, 2009
+
+Description: Abstracts the task of looking up and advertising servers and forwarders.
+
+"""
+
+include centralizedadvertise.repy
+include deserialize.py
+
+# What should we register as?
+NAT_FORWARDER_ADVERTISE_KEY = "__NAT__FORWARDER__"
+NAT_SRV_PREFIX = "__NAT_SRV__"
+
+# Limit of forwarder lookups
+NAT_MAX_LOOKUP = 50
+
+NAT_ADVERTISE_INTERVAL = 30
+NAT_ADVERTISE_TTL = 3*NAT_ADVERTISE_INTERVAL
+
+# Pools the advertisements, so that they can be done in one thread
+# Maps key-> value
+NAT_ADVERTISE_POOL = {}
+
+# Enable allows toggling of periodic advertisement
+# Run controls the advertisement thread, False will stop the thread
+NAT_ADVERTISE_STATE = {"enable":False,"run":False}
+
+# Registers the forwarder so that clients using the NATLayer can find us
+def forwarder_advertise(ip, serverport, clientport):
+  # Generate the value to advertise
+  valueDict = {"ip":ip, "server":serverport, "client":clientport}
+  value = str(valueDict)
+  
+  # Add to the advertising pool
+  NAT_ADVERTISE_POOL[NAT_FORWARDER_ADVERTISE_KEY] = value
+
+
+# Advertises a server, so that other NATLayer users can connect
+def server_advertise(key, forwarderIP, forwarderCltPort):
+  # Generate the value to advertise
+  valueDict = {"key":key, "forwarder":forwarderIP, "port":forwarderCltPort}
+  value = str(valueDict)
+
+  # Alter the key, add the prefix
+  key = NAT_SRV_PREFIX + key
+
+  # Add to the advertising pool
+  NAT_ADVERTISE_POOL[key] = value
+    
+    
+# Lookup a forwarder so that we can connect
+def forwarder_lookup():
+  # Get the list of forwarders
+  forwarders = centralizedadvertise_lookup(NAT_FORWARDER_ADVERTISE_KEY, NAT_MAX_LOOKUP)
+  
+  # Safety check..
+  if len(forwarders) == 0:
+    raise Exception, "No forwarders could be found!"
+  
+  # Grab a random forwarder
+  index = int(randomfloat() * len(forwarders))
+  
+  # Get the info
+  forwarderInfo = forwarders[index]
+  forwarderInfo = deserialize(forwarderInfo)  
+  
+  # Return a tuple containing the IP and port for server and client
+  return (forwaderInfo["ip"], forwarderInfo["server"], forwarderInfo["client"])
+
+
+# Finds a server using the NATLayer
+def server_lookup(key):
+  # Get the proper key, add the prefix
+  key = NAT_SRV_PREFIX + key
+
+  # Fetch all the keys
+  lst = centralizedadvertise_lookup(key, NAT_MAX_LOOKUP)
+  num = len(lst)
+  
+  # Safety check...
+  assert(num <= 1)
+  if num == 0:
+    raise Exception, "Host could not be found!"
+
+  # Get the information about the server
+  info = deserialize(lst[0])
+
+  # Return a tuple of the forwarder IP port
+  return (info["forwarder"], info["port"])
+
+
+# Toggles advertisement
+# Enable: allows or disallows advertisement of the pool
+# threadRun: allows/starts or stops the advertisement thread
+def nat_toggle_advertisement(enabled, threadRun=True): 
+  NAT_ADVERTISE_STATE["enable"] = enabled
+  
+  # Start the advertisement thread if necessary
+  if not NAT_ADVERTISE_STATE["run"] and threadRun:
+    settimer(.1, _nat_advertise_thread, ())
+  
+  NAT_ADVERTISE_STATE["run"] = threadRun
+
+  
+# Launch this thread with settimer to handle the advertisement  
+def _nat_advertise_thread():
+  while True:
+    # Check if advertising is enabled:
+    if NAT_ADVERTISE_STATE["enable"]:
+      # Advertise everything in the pool
+      for (key, val) in NAT_ADVERTISE_POOL.items():
+        try:
+          centralizedadvertise_announce(key, val, NAT_ADVERTISE_TTL)
+        except:
+          pass
+        
+    # Check if we should terminate
+    if not NAT_ADVERTISE_STATE["run"]:
+      break
+    
+    # Sleep for a while
+    sleep(NAT_ADVERTISE_INTERVAL)
+
