4a5e1420b63cdbc0ca2c54afcb74a01a4d96d3ea
armon <armon@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-03-28 02:10:08 +0000

    Deleted old unit tests.

--- natlayer/tests/test_0_natcon_server_init.py
+++
@@ -1,9 +0,0 @@
-include NATLayer.py
-
-# This test just attempts to create a NATConnection object, which should connect to the forwarder
-# There is no expected output
-
-if callfunc == "initialize":
-  natcon = NATConnection("000000000000", "127.0.0.1", 12345)
-  natcon.initServerConnection()
-  natcon.close()
