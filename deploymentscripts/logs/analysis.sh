45a271630d32ca30a4327c35db22710dc915ae15
sal <sal@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-03-30 05:25:55 +0000

    corrected the reference to the column

--- deploymentscripts/logs/analysis.sh
+++ deploymentscripts/logs/analysis.sh
@@ -2,7 +2,7 @@ cp 20* log
 ls 20* > results.txt
 cat log | grep "PO\!" | awk {'print $2'} > `ls 20*`listFailed
 mv 20* archive
-echo "Good:" >> results.txt; cat log | grep "GOOD" | wc | awk '{print $2}' >> results.txt
-echo "Scp failed:" >> results.txt; cat log | grep "scp " | wc | awk '{print $2}' >> results.txt
-echo "Didn't pass all of the tests:" >> results.txt; cat log | grep "PO!" | wc | awk '{print $2}' >> results.txt
+echo "Good:" >> results.txt; cat log | grep "GOOD" | wc | awk '{print $1}' >> results.txt
+echo "Scp failed:" >> results.txt; cat log | grep "scp " | wc | awk '{print $1}' >> results.txt
+echo "Didn't pass all of the tests:" >> results.txt; cat log | grep "PO!" | wc | awk '{print $1}' >> results.txt
 cat log | grep "PO\!" | awk {'print $2'}
