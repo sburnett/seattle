137c39bf4d146c70790085e575bb080884f38a00
kimbrl <kimbrl@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-05-04 16:14:09 +0000

    removed old semantics folder

--- autograder/build_mix_example.sh
+++ autograder/build_mix_example.sh
@@ -6,14 +6,14 @@ IN=nm_remote_api.mix
 OUT=nm_remote_api.py
 
 # go to svn trunk location
-cd ../../svn_seattle/trunk
+cd ..
 
 # dump repo into directory (auto_build directory MUST exist in trunk,
 #                           just mkdir auto_build)
 python preparetest.py auto_build
 
 # copy .mix file to this directory
-cp ../../autograder_stuff/attempt2_clean/$IN auto_build
+cp autograder/$IN auto_build/$IN
 
 # go into the build directory
 cd auto_build
@@ -21,5 +21,7 @@ cd auto_build
 # run it through the preprocessor
 python repypp.py $IN $OUT
 
+
 # place script into a seattle installation (seattle_repy/)
-cp $OUT ../../../autograder_stuff/attempt2_clean/repy_code/
+#cp $OUT ../../../autograder_stuff/attempt2_clean/repy_code/
+cp $OUT ../autograder/emulab/
