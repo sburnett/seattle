d4b9b0629f28ab825a3e33643236c20d3f060077
zackrb <zackrb@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-05-09 04:13:24 +0000

    Committing test code test_announcing.py for repy service time_server.repy

---
+++ timeserver/tests/test_announcing.py
@@ -0,0 +1,47 @@
+# Tests to make sure that time_server.repy is announcing itself properly
+# to the central server over a period of 15 minutes (because time_server.repy
+# re-announces itself every four to five minutes.
+
+
+include centralizedadvertise.repy
+
+
+if callfunc == "initialize":
+
+  if len(callargs) > 1:
+    raise Exception("Too many call arguments")
+
+  elif len(callargs) == 1:
+    port = int(callargs[0])
+    ip = getmyip()
+
+  else:
+    raise Exception("Need port number")
+
+  print "This test performs a series of 15 tests to make sure the service time_server.repy is properly, and continually, announcing to the central server"
+
+  success = True
+  print "Performing test 1 :"
+  for x in range(2,17):
+    while True:
+      try:
+        ipandport = centralizedadvertise_lookup("time_server")
+      except Exception,e:
+        if "Interrupted system call" in str(e):
+          print "Interrupted system call, retrying test..."
+        else:
+          print "TEST FAILED on try ",x-1,": ",e
+          success = False
+          break
+      else:
+        print "time_server.repy successfully announced the following ip and port:"
+        print "remote ip: ",ipandport[0]
+        print "remote port: ",ipandport[1]
+        print "Test ",x-1," complete"
+        if x != 16:
+          print "Performing test ",x,":"
+          sleep(60)
+        break
+
+  if success:
+    print "All tests succeeded!"
