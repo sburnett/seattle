e497a318b69ff4aa69302e8b250d97703b0217bf
justinc <justinc@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-06-20 00:30:12 +0000

    We need to wait until we stop the stopper before we start the node manager and software updater.

--- dist/win/scripts/start_seattle.bat
+++ dist/win/scripts/start_seattle.bat
@@ -1,4 +1,4 @@
 @echo off
-start /min pythonw.exe get_seattlestopper_lock.py
+start /min /wait pythonw.exe get_seattlestopper_lock.py
 start /min pythonw.exe nmmain.py
 start /min pythonw.exe softwareupdater.py
