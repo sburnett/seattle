ac7ec1b0fa1a11d9460989d468f0c42cc3c626be
monzum <monzum@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-06-23 23:30:08 +0000

    edited www/deploy_state_transitions.py so a crontab is setup upon running the script

--- www/deploy_state_transitions.py
+++ www/deploy_state_transitions.py
@@ -35,27 +35,104 @@ except ImportError:
   sys.exit(0)
 
 def main():
-  # this is for preparetest to work
+
+  checkArgs()
+
+  #setup some variables for cron setup
+  cron_setup = False
+  log_dir = ""
+
+  # -c means we have to setup the crontab with a new cron job
+  if sys.argv[1] == '-c':
+    cron_setup = True
+    sys.argv = sys.argv[1:]
+    checkArgs(cron_setup)
+    log_dir = sys.argv[2]
+    
+    if not( os.path.isdir(log_dir) ):
+      help_exit("given log_foldername is not a directory")
+	  
+  #set up all the variables about directory information
   current_dir = os.getcwd()
-  os.chdir(current_dir + "/../")
+  
+  #ensures that the backslashes and forward slashes are proper for the OS
+  target_dir = os.path.normpath(sys.argv[1])
+
+  #make sure svn is up to date and start up preparetest
+  os.chdir(os.path.normpath(current_dir + "/../trunk"))
+  os.system("svn up")
+  os.chdir(current_dir)
   preparetest.main()
 
-  target_dir = sys.argv[1]
-  
-  # set working directory back to the folder we've started in
+  if not( os.path.isdir(target_dir) ):
+    help_exit("given foldername is not a directory")
+
+  #change to target directory and clean up the target directory folder
+  os.chdir(target_dir)
+
+  files_to_remove = glob.glob("*")
+
+  for f in files_to_remove:
+    if os.path.isdir(f):
+      shutil.rmtree(f)
+    else:
+      os.remove(f)
+
   os.chdir(current_dir)
 
-  # copy files to target dir
-  preparetest.copy_to_target("node_state_transitions/*", target_dir)
-
+  #the next few lines is necessary unless the file is manually copied over
+  preparetest.copy_to_target("../trunk/www/node_state_transitions/*", target_dir)
+  
   # set working dir to target
   os.chdir(target_dir)
   
   # call the process_mix function to process all mix files in the target directory
   preparetest.process_mix("repypp.py")
 
-  # go back to root project directory
-  os.chdir(current_dir) 
+  #check to see if cron setup was requested, if yes run cron_setup
+  if cron_setup:
 
+    #create the absolute path for the log file and the file needed for the 
+    #cron job
+    cron_tab_dir=os.path.normpath(current_dir + "/" + target_dir)
+    cron_log_dir=os.path.normpath(current_dir + "/" + log_dir)
+    
+    cron_line="@reboot /usr/bin/python " + cron_tab_dir + "/donationtocanonical.py >> " + cron_log_dir + "/cron_log.donationtocanonical 2>&1" + os.linesep
+
+    #setup the cron job
+    setup_crontab.add_crontab(cron_line, "donationtocanonical")
+	
+	cron_line="@reboot /usr/bin/python " + cron_tab_dir + "/canonicaltoonepercent_manyevents.py >> " + cron_log_dir + "/cron_log.canonicaltoonepercent_manyevents 2>&1" + os.linesep
+
+    #setup the cron job
+    setup_crontab.add_crontab(cron_line, "canonicaltoonepercent_manyevents")
+	
+	cron_line="@reboot /usr/bin/python " + cron_tab_dir + "/onepercenttoonepercent_manyevents.py >> " + cron_log_dir + "/cron_log.onepercenttoonepercent_manyevents 2>&1" + os.linesep
+
+    #setup the cron job
+    setup_crontab.add_crontab(cron_line, "onepercenttoonepercent_manyevents")
+
+
+
+ 
+#checks to make sure that the right amount of arguments are provided 
+def checkArgs(cron=False):
+
+  if cron and len(sys.argv) < 3:
+    help_exit("Invalid number of arguments with option -c, need <log_directory>")
+    
+  elif len(sys.argv) < 2:
+    help_exit("Invalid number of arguments")
+   
+  
+
+#exit program with a message and help menu
+def help_exit(exit_msg):
+  print exit_msg
+  option_message = "Options:\n -c\tsetup crontab"
+  print "python deploy_state_transitions.py [option] <target_directory> [<log_directory>]"
+  print option_message
+  sys.exit(1)
+  
 if __name__ == '__main__':
   main()
