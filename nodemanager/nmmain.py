c5041374aa4ccf0230a7171b9065fe6c76453ee0
justinc <justinc@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-06-24 01:32:29 +0000

    I've changed the version of the node manager in nmmain.py to be "testing" to avoid confusing our developers.

--- nodemanager/nmmain.py
+++ nodemanager/nmmain.py
@@ -114,7 +114,7 @@ LOG_AFTER_THIS_MANY_ITERATIONS = 600  # every 10 minutes
 # BUG: what if the data on disk is corrupt?   How do I recover?   What is the
 # "right thing"?   I could run nminit again...   Is this the "right thing"?
 
-version = "0.1e"
+version = "testing"
 
 # Our settings
 configuration = {}
