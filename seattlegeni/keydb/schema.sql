5224a7ae03c887a8954143f4e4d76b615f8c793e
jsamuel <jsamuel@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-08-17 20:32:48 +0000

    In the seattlegeni keydb schema, increase the length of the keydescription field so it can accommodate a node id (that is, a key).

--- seattlegeni/keydb/schema.sql
+++ seattlegeni/keydb/schema.sql
@@ -1,9 +1,9 @@
 /*
  * This creates a MyISAM table. MyISAM doesn't support transactions. We don't
- * need transactions because we have make very simple single-query database
+ * need transactions because we only make very simple single-query database
  * writes.
  */
-CREATE TABLE `keys` (
+CREATE TABLE IF NOT EXISTS `keys` (
   `id` int(10) unsigned NOT NULL auto_increment,
   /* `pubkeyhash` is never read by our code. We only use it to ensure unique
    * public keys, and the `pubkey` field is too long to specify as unique
@@ -11,7 +11,8 @@ CREATE TABLE `keys` (
   `pubkeyhash` varchar(32) NOT NULL,
   `pubkey` varchar(2048) NOT NULL,
   `privkey` varchar(4096) NOT NULL,
-  `description` varchar(255) NOT NULL,
+  `description` text NOT NULL,
   PRIMARY KEY  (`id`),
-  UNIQUE KEY (`pubkeyhash`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  UNIQUE KEY `pubkeyhash` (`pubkeyhash`),
+  KEY `description` (`description`(764))
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 ;
