96f42343df3472baa646ed5805c2451d8a06de0d
zackrb <zackrb@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-09-10 23:03:45 +0000

    Committing the Windows script wrappers to the repository (found in trunk/dist/script_wrappers/win/).

---
+++ dist/script_wrappers/win/uninstall.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Change into the seattle_repy directory.
+FOR /F "tokens=*" %%A IN ('cd') DO SET OrigDir=%%A
+SET TargetPath=%~f0
+SET seattle_repyDir=%TargetPath:uninstall.bat=seattle_repy%
+cd %seattle_repyDir%
+
+uninstall.bat
+
+cd %OrigDir%
