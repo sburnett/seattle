d1998481f7a50ac44cca61243c3f9377411b2011
jsamuel <jsamuel@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-10-06 18:14:44 +0000

    Added scripts to dist/ that I use to deploy new versions on blackbox. I also modified update_software.py which does not appear to be used anywhere else and was not maintained (I use it in one of the new scripts being checked in).

--- dist/update_software.py
+++ dist/update_software.py
@@ -16,10 +16,17 @@
 import sys
 import make_base_installers
 
-UPDATE_SITE = "/home/couvb/public_html/updatesite"
+
+
+
+
 DEBUG = False
 
-def update(trunk_location, pubkey, privkey):
+
+
+
+
+def update(trunk_location, pubkey, privkey, update_dir):
   """
   <Purpose>
     Populates the update directory (set by a constant) with the
@@ -36,6 +43,9 @@ def update(trunk_location, pubkey, privkey):
     privkey:
       The private key used to generate the metafile.
     
+    updatedir:
+      The directory to put the generated update files in.
+    
   <Exceptions>
     IOError on bad filepath.
   
@@ -45,23 +55,35 @@ def update(trunk_location, pubkey, privkey):
   <Returns>
     None.
   """
-  update_dir = UPDATE_SITE + "/real"
   # If we're in DEBUG mode, don't use the real update
   # directory
   if DEBUG:
     print "Debug mode..."
-    update_dir = UPDATE_SITE + "/test"
-    make_base_installers.prepare_gen_install_files(trunk_location, update_dir,
-                                                   False, pubkey, privkey, False)
+    update_dir = update_dir + "/test"
+    
+  make_base_installers.prepare_gen_files(trunk_location, update_dir,
+                                         False, pubkey, privkey, False)
+
+
+
+
 
 def main():
   global DEBUG
-  if len(sys.argv) < 2:
-    print "usage: python update_software.py trunk/location/ publickey privatekey [-d]"
-  else:
-    if sys.argv[4] == "-d":
+  
+  if len(sys.argv) < 3:
+    print "usage: python update_software.py trunk/location/ publickey privatekey updatedir [-d]"
+    
+  elif len(sys.argv) == 6:
+    if sys.argv[5] == "-d":
       DEBUG = True
-      update(sys.argv[1], sys.argv[2], sys.argv[3])
       
+  update(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+
+
+
+
 if __name__ == "__main__":
   main()
+
