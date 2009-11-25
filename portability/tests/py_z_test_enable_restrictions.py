f8519d1114703d59bb0b587748a3b4c3514cfd84
justinc <justinc@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-11-25 00:28:19 +0000

    Fixes for #765.   This wasn't a quick fix because the tests needed to be changed to flush their output.

--- portability/tests/py_z_test_enable_restrictions.py
+++ portability/tests/py_z_test_enable_restrictions.py
@@ -11,7 +11,12 @@ import repyhelper
 override_restrictions()
 enable_restrictions()
 
+import sys
 
 # in the call to this, it will do 'exitall' if the restrictions are in place
 repyhelper.translate_and_import("rhtest_printifreadisfast.repy")
 
+
+# We need to flush stdout (this won't be done for us unless it goes to the 
+# terminal).
+sys.stdout.flush()
--- portability/tests/py_z_test_initialize_restrictions.py
+++ portability/tests/py_z_test_initialize_restrictions.py
@@ -9,7 +9,12 @@ initialize_restrictions("restrictions.veryslowread")
 import repyhelper
 
 
+import sys
 
 # in the call to this, it will do 'exitall' if the restrictions are in place
 repyhelper.translate_and_import("rhtest_printifreadisfast.repy")
 
+
+# We need to flush stdout (this won't be done for us unless it goes to the 
+# terminal).
+sys.stdout.flush()
