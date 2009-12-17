b311647de6a935064aa03fcdcd038486cec42245
jsamuel <jsamuel@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2009-12-17 01:22:10 +0000

    Added script for deploying the advertise server.

---
+++ advertiseserver/deploy_advertiseserver.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+TRUNK_DIR=trunk
+DEPLOY_TO_DIR=advertiseserver_deployed
+
+if [ ! -d $TRUNK_DIR ]; then
+  echo "TRUNK_DIR doesn't exist: $TRUNK_DIR"
+  exit 1
+fi
+
+if [ -d $DEPLOY_TO_DIR ]; then
+  backupname=$DEPLOY_TO_DIR.bak.`date +%s`
+  echo "Backing up $DEPLOY_TO_DIR as $backupname"
+  mv $DEPLOY_TO_DIR $backupname || exit 1
+fi
+
+echo "Making directory $DEPLOY_TO_DIR"
+mkdir $DEPLOY_TO_DIR || exit 1
+
+echo "Copying files to $DEPLOY_TO_DIR. Expect a few messages about omitting directories."
+cp $TRUNK_DIR/advertiseserver/* $DEPLOY_TO_DIR/
+cp $TRUNK_DIR/repy/* $DEPLOY_TO_DIR/
+cp $TRUNK_DIR/seattlelib/* $DEPLOY_TO_DIR/
+cp $TRUNK_DIR/nodemanager/servicelogger.mix $DEPLOY_TO_DIR/
+
+echo "repypp'ing mix files"
+cd $DEPLOY_TO_DIR
+python repypp.py servicelogger.mix servicelogger.py
+python repypp.py advertiseserver.mix advertiseserver.py
+
+echo "Done."
+echo "Use the following commands to start the advertise server:"
+echo "#########################################"
+echo "cd $DEPLOY_TO_DIR"
+echo "python repy.py restrictions.advertiseserver advertiseserver.py >> log.stdout 2>> log.stderr < /dev/null"
+echo "#########################################"
+
