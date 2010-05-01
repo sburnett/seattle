96e4442e9e34a4a123412cf68ba1e067e0443b05
meagher <meagher@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2010-05-01 22:41:57 +0000

    Added port command-line argument and provided link to wiki documentation.

--- geoip_server/geoip_server.py
+++ geoip_server/geoip_server.py
@@ -11,12 +11,15 @@
   queries using the pygeoip library.
 
 <Usage>
-  geoip_server.py /path/to/GeoIP.dat
+  python geoip_server.py /path/to/GeoIP.dat PORT
 
-  Where /path/to/GeoIP.dat is the path to a legal GeoIP database. Databases can
-  be downloaded at http://www.maxmind.com/app/ip-location.
+  Where /path/to/GeoIP.dat is the path to a legal GeoIP database and PORT is
+  the port on which to host the server. Databases can be downloaded at
+  http://www.maxmind.com/app/ip-location.
 
   More information on pygeoip at http://code.google.com/p/pygeoip/.
+  More information on geoip_server.py at
+  http://seattle.cs.washington.edu/wiki/GeoIPServer
 """
 
 import sys
@@ -26,17 +29,18 @@ import repyportability
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 # Handle arguments
-if len(sys.argv) < 2:
-    print "Error: Please specify the filename of a valid GeoIP datebase."
+if len(sys.argv) < 3:
+    print "Usage: python geoip_server.py /path/to/GeoIP.dat PORT"
     raise RuntimeError
 
 geoipdb_filename = sys.argv[1]
+port = int(sys.argv[2])
 
 # Get external IP
 ext_ip = repyportability.getmyip()
 
 # Create server
-server = SimpleXMLRPCServer((ext_ip, 12679), allow_none=True)
+server = SimpleXMLRPCServer((ext_ip, port), allow_none=True)
 
 # Initialize and register geoip object
 gic = pygeoip.GeoIP(geoipdb_filename)
