ce5bda691e2b4eac8ebfaedde8ac5b4b083faedd
monzum <monzum@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2010-06-18 20:55:59 +0000

    Fix to #893. Modified test_time_tcp.py to provide more information when the test fails, fixed the test so notifications are sent only when the time difference is more then 10 seconds. Also modified seattlelib/tcp_time.rep so the call tcp_time_updatetime() now returns the ip:port of the server that it managed to connect to on success.

--- integrationtests/time/test_time_tcp.py
+++ integrationtests/time/test_time_tcp.py
@@ -15,6 +15,9 @@
 import sys
 import send_gmail
 import integrationtestlib
+import traceback
+import time
+import math
 
 # use repy helper to bring in tcp_time.repy
 import repyhelper
@@ -36,33 +39,64 @@ def main():
   integrationtestlib.notify_list.append("lekimbrel@gmail.com")
     
   integrationtestlib.log("Starting test_time_tcp.py...")  
+
+  # Whenever we fail to do tcp_time_updatetime, we add the exception string
+  exception_string_list = []
   
   # get the time 5 times and make sure they are reasonably close
   test_start = getruntime()
   times = []
+ 
+  # Keep a list of servers we connected to or tried to connect to for time.
+  server_list = []
+
+  # Connect to 5 servers and retrieve the time.
   for i in range (5):
     try:
-      tcp_time_updatetime(12345)
-      times.append(time_gettime())
+      connected_server = tcp_time_updatetime(12345)
+      current_time = time_gettime()
+      times.append(current_time)
+      server_list.append(connected_server)
+      integrationtestlib.log("Calling time_gettime(). Retrieved time: " + str(current_time))
     except Exception,e:
+      exception_string_list.append({'Server' : connected_server, 'Exception' : str(e), 'Traceback' : str(traceback.format_exc())})
       pass
+
+  # Get the stop time 
   test_stop1 = getruntime()
 
-  
-  if len(times) < 4:  # more than one attempt failed
-    notify_str += 'failed to get ntp time via tcp at least twice in 5 attempts'
 
+  # Make sure that we don't fail too many times
+  if len(times) < 4:  # more than one attempt failed
+    notify_str += "failed to get ntp time via tcp at least twice in 5 attempts\n\n"
+    notify_str += "Appending a list of all the exceptions returned and servers we attempted to connect to:\n\n"
+    notify_str += str(exception_string_list)
+
+  fail_test_str = ''
+
+  # Find out the difference in time between the start time and end time of the test.
   diff = max(times) - min(times)
-  if diff > (test_stop1 - test_start):  
-    integrationtestlib.log('WARNING large descrepancy between tcp times: '+str(diff))
-    notify_str += ' WARNING large descrepancy between tcp times: '+str(diff)
-    
-  
+  test_diff = test_stop1 - test_start
+
+  # If the time difference was more then 10 seconds then there is a problem.
+  if math.fabs(diff - test_diff) > 10:  
+    fail_test_str += ' WARNING large descrepancy between tcp times. \nThe difference between tcp_time diff and the actual test diff is: '+str(diff - test_diff)
+    fail_test_str += "\n\nThe start time of test was: " + str(test_start) + ". The end time of test was: " + str(test_stop1)
+    fail_test_str += "\n\nThe list of times returned by the tcp_server were: " + str(times)
+    fail_test_str += "\n\nThe max time was: " + str(max(times)) + ". The min time was: " + str(min(times))
+    fail_test_str += ". \n\nThe tcp diff time was: " + str(diff) + ". This time should have been less then: " + str(test_diff)
+    fail_test_str += "\n\nThe servers we connected to are: " + str(server_list) 
+    notify_str += fail_test_str
+    integrationtestlib.log(fail_test_str)
+    integrationtestlib.log("Local time diff: " + str(local_end_time - local_start_time))    
+
+  # Now do an ntp time test
   try:
     ntp_time_updatetime(12345)
     ntp_t = time_gettime()
   except Exception,e:
-    fail_test(str(e))
+    integrationtestlib.log("Failed to call ntp_time_updatetime(). Returned with exception: " +str(e))
+    notify_str += "\nFailed to call ntp_time_updatetime(). Returned with exception: " +str(e)
   
   test_stop2 = getruntime()
   diff = ntp_t - max(times)
