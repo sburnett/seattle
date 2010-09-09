18f2df8a7db9cf2f36003cd18801ab306ca077dd
monzum <monzum@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2010-09-09 23:17:05 +0000

    Modified preparetest.py from copying over the shim files as there seems to be some bug in the new nmmain.py patch.

--- preparetest.py
+++ preparetest.py
@@ -164,11 +164,12 @@ def main():
   # The license must be included in anything we distribute.
   copy_to_target("LICENSE.TXT", target_dir)
 
+  # Uncomment this when its ready to be in production.
   # Copy over the files needed for using shim.
-  copy_to_target("production_nat_new/src/nmpatch/nmmain.py", target_dir)
-  copy_to_target("production_nat_new/src/nmpatch/nmclient.repy", target_dir)
-  copy_to_target("production_nat_new/src/nmpatch/sockettimeout.repy", target_dir)
-  copy_to_target("production_nat_new/src/nmpatch/ShimStackInterface.repy", target_dir)  
+  # copy_to_target("production_nat_new/src/nmpatch/nmmain.py", target_dir)
+  # copy_to_target("production_nat_new/src/nmpatch/nmclient.repy", target_dir)
+  # copy_to_target("production_nat_new/src/nmpatch/sockettimeout.repy", target_dir)
+  # copy_to_target("production_nat_new/src/nmpatch/ShimStackInterface.repy", target_dir)  
   
   # Only copy the tests if they were requested.
   if repytest:
