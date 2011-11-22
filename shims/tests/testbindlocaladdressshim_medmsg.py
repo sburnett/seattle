97800b7119803e985088336b477e97b0c27d2ee6
monzum <monzum@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2011-11-22 20:31:57 +0000

    Commited BindLocalAddressShim with its test files.

---
+++ shims/tests/testbindlocaladdressshim_medmsg.py
@@ -0,0 +1,20 @@
+dy_import_module_symbols("testgeneralshim_helper")
+
+# Choose a random ip address and use BindLocalAddressShim to change to 
+# the correct ip address, and it should work.
+
+SERVER_IP = "145.65.8.245"
+SERVER_PORT = 72689
+
+SERVER_SHIM_STRING = "(CoordinationShim)(BindLocalAddressShim,%s:%d)" % (getmyip(), 34256)
+CLIENT_SHIM_STRING = "(CoordinationShim)"
+
+
+# Test data size of 10 MB
+DATA_TO_SEND = "HelloWorld" * 1024 * 1024
+
+RECV_SIZE = 2**14 # 16384 bytes.
+END_TAG = "@@END"
+SLEEP_TIME = 0.01
+
+launch_test()
