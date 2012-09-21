1ba545a4904aeaf9683602ddda835d129d302515
gpress <gpress@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2012-09-21 02:40:52 +0000

    fix for #1104, this disables the submit button client side on the register page after it has been pressed preventing multiple submissions.  This also adds a loading animation.

--- seattlegeni/website/html/media/scripts/register.js
+++ seattlegeni/website/html/media/scripts/register.js
@@ -1,6 +1,10 @@
-
+/*
+	Edited by: Gaetano Pressimone
+	Last Modfied: Sept 20 2012
+*/
 $(document).ready(function() {
 	toggle_upload();
+	get_resources_loading_indicator();
 	$("#id_gen_upload_choice").change(toggle_upload);
 });
 
@@ -10,4 +14,11 @@ function toggle_upload() {
 	} else {
 		$("#uploadkey").show();
 	}
+}
+/* Disables submit button on press and shows the loading icon */
+function get_resources_loading_indicator() {
+	$("#middle > form").submit(function () {
+		$('input:submit').attr("disabled", true);
+		$("#loading_indicator").css('visibility', 'visible');
+	});
 }
