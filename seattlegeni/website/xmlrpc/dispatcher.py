d9cfb18c865e64f4335147e0912a2fcc61843fbe
gpress <gpress@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2013-01-11 21:45:56 +0000

    addresses tickets #1140 and #1142 fixing 'Clearinghouse XMLRPC interface cannot be reached' error.

--- seattlegeni/website/xmlrpc/dispatcher.py
+++ seattlegeni/website/xmlrpc/dispatcher.py
@@ -22,12 +22,12 @@ from SimpleXMLRPCServer import SimpleXMLRPCDispatcher
 from django.http import HttpResponse
 
 from seattlegeni.website.xmlrpc.views import PublicXMLRPCFunctions
-
+from django.views.decorators.csrf import csrf_exempt
 
 
 # This is the url that will be displayed if the xmlrpc service is requested
 # directory through a web browser (that is, through a GET request).
-SEATTLECLEARINGHOUSE_XMLRPC_API_DOC_URL = "https://seattle.cs.washington.edu/wiki/SeattleGeniAPI"
+SEATTLECLEARINGHOUSE_XMLRPC_API_DOC_URL = "https://seattle.cs.washington.edu/wiki/SeattleGeniApi"
 
 # Create a Dispatcher. This handles the calls and translates info to function maps.
 # TODO: allow_none = True or False? Does using None in the api make the xmlrpc
@@ -35,7 +35,7 @@ SEATTLECLEARINGHOUSE_XMLRPC_API_DOC_URL = "https://seattle.cs.washington.edu/wik
 dispatcher = SimpleXMLRPCDispatcher(allow_none=False, encoding=None)
 
 
-
+@csrf_exempt
 def rpc_handler(request):
   """
   All xmlrpc requests are initially routed here by django. The actual functions we
@@ -56,7 +56,7 @@ def rpc_handler(request):
     response.write(dispatcher._marshaled_dispatch(request.raw_post_data))
   else:
     response.write("<b>This is the SeattleGeni XML-RPC Service.</b><br>")
-    response.write("Please see " + SEATTLECLEARINGHOUSE_XMLRPC_API_DOC_URL + " for more information.")
+    response.write("Please see <a href=" + SEATTLECLEARINGHOUSE_XMLRPC_API_DOC_URL + ">" + SEATTLECLEARINGHOUSE_XMLRPC_API_DOC_URL + "</a> for more information.")
 
   response['Content-length'] = str(len(response.content))
   return response
