4c4e156ee815f3862211b82a0ba6cd386ef85f73
monzum <monzum@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2013-05-07 19:11:20 +0000

    Updated nodemanager and added a functionality to turn on/off affixes in Seattle.

---
+++ affix_seattle/stop_seattle_affix.py
@@ -0,0 +1,22 @@
+from repyportability import *
+_context = locals()
+add_dy_support(_context)
+
+dy_import_module_symbols("advertise.repy")
+
+try:
+    advertise_announce("SeattleAffixStack", "(NoopShim)", 600)
+except AdvertiseError, e:
+    print "Advertise error: " + str(e)
+
+lookup_string = advertise_lookup("SeattleAffixStack")
+print "Current lookup string is: " + str(lookup_string)
+
+try:
+    advertise_announce("EnableSeattleAffix", "False", 600)
+except AdvertiseError, e:
+    print "Advertise error: " + str(e)
+
+
+lookup_string = advertise_lookup("EnableSeattleAffix")
+print "EnableSeattleAffix flag set to: " + str(lookup_string)
