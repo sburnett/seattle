3cfdb089d06487b5141b7ff1ca136e696082e55f
leonwlaw <leonwlaw@1c8a4c40-6063-4349-96e8-9cae12fcc912>
2014-02-05 22:15:56 +0000

    Update lookup node states integration test.  * Moved definition of AdvertiseLookup exception to address NameError that gets raised  * Changed all references from old state keys to new ones  ** onepercentmanyevents -> twopercent  ** movingtoonepercentmanyevents -> movingtotwopercent  * Removes check that uses openDHT

--- integrationtests/lookup_node_states/test_lookup_node_states.py
+++ integrationtests/lookup_node_states/test_lookup_node_states.py
@@ -4,8 +4,8 @@
 
 <Purpose>
   use advertise.repy to do a lookup for nodes in the four
-  differe states: acceptdonation, canonical, onepercentmanyevents
-  and movingtoonepercentmanyevents. And send out notifications
+  differe states: acceptdonation, canonical, twopercent
+  and movingto_twopercent. And send out notifications
   if the number of nodes in each state is not what is expected.
 
 <Started> 
@@ -24,12 +24,16 @@ repyhelper.translate_and_import('rsa.repy')
 repyhelper.translate_and_import('advertise.repy')
 
 #get all public keys for different states from file
-onepercent_manyevent_pubkey = rsa_file_to_publickey("/home/integrationtester/cron_tests/lookup_node_states/onepercentmanyevents.publickey")
+twopercentpublickey = rsa_file_to_publickey("/home/integrationtester/cron_tests/lookup_node_states/twopercent.publickey")
 canonicalpublickey = rsa_file_to_publickey("/home/integrationtester/cron_tests/lookup_node_states/canonical.publickey")
 acceptdonationpublickey = rsa_file_to_publickey("/home/integrationtester/cron_tests/lookup_node_states/acceptdonation.publickey")
-movingtoonepercentpublickey = rsa_file_to_publickey("/home/integrationtester/cron_tests/lookup_node_states/movingtoonepercent.publickey")
+movingtotwopercentpublickey = rsa_file_to_publickey("/home/integrationtester/cron_tests/lookup_node_states/movingto_twopercent.publickey")
 
-important_node_states = [('onepercent_manyevent', onepercent_manyevent_pubkey), ('canonical', canonicalpublickey), ('acceptdonation', acceptdonationpublickey), ('movingtoonepercent', movingtoonepercentpublickey)] 
+important_node_states = [('twopercent', twopercentpublickey), ('canonical', canonicalpublickey), ('acceptdonation', acceptdonationpublickey), ('movingto_twopercent', movingtotwopercentpublickey)] 
+
+#exception raised if AdvertiseLookup fails somehow
+class AdvertiseLookup(Exception):
+  pass
 
 
 def check_nodes(server_lookup_type):
@@ -64,7 +68,7 @@ def check_nodes(server_lookup_type):
     try:
       node_list = advertise_lookup(node_state_pubkey, maxvals = 10*1024*1024, lookuptype=[server_lookup_type])
     except:
-      raise AdvertiseLookup, "advertise_lookup() failed when looking up key: "+ node_state_pubkey + " for "+server_lookup_type
+      raise AdvertiseLookup("advertise_lookup() failed when looking up key: "+ rsa_publickey_to_string(node_state_pubkey) + " for "+server_lookup_type)
 
     #keep track of total nodes
     total_nodes+=len(node_list)
@@ -107,8 +111,8 @@ def main():
 
   max_acceptdonation_nodes = 50 
   max_canonical_nodes = 50 
-  max_movingtoonepercent = 20
-  min_onepercentmanyevents_nodes = 300 
+  max_movingtotwopercent = 20
+  min_twopercent_nodes = 300 
 
   central_results = check_nodes('central')
   integrationtestlib.log("Lookup results for central: "+ str(central_results))
@@ -125,25 +129,12 @@ def main():
     print message
     integrationtestlib.notify(message, notification_subject)
 
-  elif central_results['onepercent_manyevent'] < min_onepercentmanyevents_nodes:
-    message="Too few nodes in onepercentmanyevents state: "+str(central_results['onepercent_manyevent'])+"\nResults from 'central' server:\n"+str(central_results)
+  elif central_results['twopercent'] < min_twopercent_nodes:
+    message="Too few nodes in twopercent state: "+str(central_results['twopercent'])+"\nResults from 'central' server:\n"+str(central_results)
     print message
     integrationtestlib.notify(message, notification_subject)
 
-  opendht_results = check_nodes('opendht')
-
-  print opendht_results  
-   
 
 if __name__ == "__main__":
   main()
 
-
-
-
-
-
-
-#exception raised if AdvertiseLookup fails somehow
-class AdvertiseLookup(Exception):
-  pass
